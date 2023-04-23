7f043a82b5bd5ee288262b7bbd840a71e9ed75ac
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-23 08:05:57 +0545

    Update work with numpy.py

--- work with numpy.py
+++ work with numpy.py
@@ -23,6 +23,7 @@ print(type(np_baseball))
 # Multiply np_height_in with 0.0254 to convert all height measurements from inches to meters. Store the new values in a new array, np_height_m.
 # Print out np_height_m and check if the output makes sense.
 
+#solution:
 # Import numpy
 import numpy as np
 
@@ -45,6 +46,7 @@ print(np_height_m)
 # Save the resulting numpy array as bmi.
 # Print out bmi.
 
+#solution:
 # height_in and weight_lb are available as regular lists
 
 # Import numpy
@@ -68,6 +70,7 @@ print(bmi)
 # Print the array light.
 # Print out a numpy array with the BMIs of all baseball players whose BMI is below 21. Use light inside square brackets to do a selection on the bmi array.
 
+#solution:
 # height_in and weight_lb are available as a regular lists
 
 # Import numpy
@@ -93,6 +96,7 @@ print(bmi[light])
 # Subset np_weight_lb by printing out the element at index 50.
 # Print out a sub-array of np_height_in that contains the elements at index 100 up to and including index 110.
 
+#Solution:
 # height_in and weight_lb are available as a regular lists
 
 # Import numpy
@@ -115,6 +119,8 @@ print(np_height_in[100:111])
 # Print out the type of np_baseball.
 # Print out the shape attribute of np_baseball. Use np_baseball.shape.
 
+
+#solution:
 # Create baseball, a list of lists
 baseball = [[180, 78.4],
             [215, 102.7],
@@ -137,6 +143,9 @@ print(np_baseball.shape)
 
 # Use np.array() to create a 2D numpy array from baseball. Name it np_baseball.
 # Print out the shape attribute of np_baseball.
+
+
+#solution:
 # baseball is available as a regular list of lists
 
 # Import numpy package
@@ -149,6 +158,28 @@ np_baseball=np.array(baseball)
 print(np_baseball.shape)
 
 
+# Print out the 50th row of np_baseball.
+# Make a new variable, np_weight_lb, containing the entire second column of np_baseball.
+# Select the height (first column) of the 124th baseball player in np_baseball and print it out.
+
+
+# solution:
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (2 cols)
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+
+print(np_baseball[49,:])
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb=np_baseball[:,1]
+
+# Print out height of 124th player
+print(np_baseball[123,0])
 
 
 
