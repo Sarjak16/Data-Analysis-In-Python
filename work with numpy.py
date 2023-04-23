11b7df3be60a804b697cfde95495f47f5d19746b
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-23 07:42:36 +0545

    Update work with numpy.py

--- work with numpy.py
+++ work with numpy.py
@@ -37,3 +37,36 @@ np_height_m = np_height_in * 0.0254
 
 # Print np_height_m
 print(np_height_m)
+
+#EXERCISE 3
+# Create a numpy array from the weight_lb list with the correct units. Multiply by 0.453592 to go from pounds to kilograms. Store the resulting numpy array as np_weight_kg.
+# Use np_height_m and np_weight_kg to calculate the BMI of each player. Use the following equation:
+ 
+# Save the resulting numpy array as bmi.
+# Print out bmi.
+
+# height_in and weight_lb are available as regular lists
+
+# Import numpy
+import numpy as np
+
+# Create array from height_in with metric units: np_height_m
+np_height_m = np.array(height_in) * 0.0254
+
+# Create array from weight_lb with metric units: np_weight_kg
+np_weight_kg=np.array(weight_lb)*0.453592
+
+# Calculate the BMI: bmi
+bmi = np_weight_kg / np_height_m ** 2
+
+# Print out bmi
+print(bmi)
+
+
+
+
+
+
+
+
+
