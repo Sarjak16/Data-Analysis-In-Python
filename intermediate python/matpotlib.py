c38092da9c03ca0d55f235ecaf2a836a6d6447ed
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-24 09:10:10 +0545

    Update matpotlib.py

--- intermediate python/matpotlib.py
+++ intermediate python/matpotlib.py
@@ -106,6 +106,23 @@ plt.hist(life_exp, bins = 20)
 plt.show()
 plt.clf()
 
+#ex3:
+# Build a histogram of life_exp with 15 bins.
+# Build a histogram of life_exp1950, also with 15 bins. Is there a big difference with the histogram for the 2007 data?
+
+# Histogram of life_exp, 15 bins
+plt.hist(life_exp,15)
+
+# Show and clear plot
+plt.show()
+plt.clf()
+
+# Histogram of life_exp1950, 15 bins
+plt.hist(life_exp1950,15)
+
+# Show and clear plot again
+plt.show()
+plt.clf
 
 
 
