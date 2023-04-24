6e48f72c97c54401c1f1d4c54b09830e179623e5
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-24 08:53:15 +0545

    Update matpotlib.py

--- intermediate python/matpotlib.py
+++ intermediate python/matpotlib.py
@@ -33,3 +33,20 @@ plt.plot(gdp_cap, life_exp)
 
 # Display the plot
 plt.show()
+
+# Scatterplot..............................................................................................................................
+# Change the line plot that's coded in the script to a scatter plot.
+# A correlation will become clear when you display the GDP per capita on a logarithmic scale. Add the line plt.xscale('log').
+# Finish off your script with plt.show() to display the plot.
+
+#Solution:
+# Change the line plot below to a scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale('log')
+
+# Show plot
+plt.show()
+
+
