8e1cef68a72bd7366d30575f1b83fca30287eab0
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-25 21:00:05 +0545

    Update dictionaries.py

--- intermediate python/dictionaries.py
+++ intermediate python/dictionaries.py
@@ -55,13 +55,47 @@ europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo
 europe["italy"]='rome'
 
 # Print out italy in europe
-print(europe['italy'])
+print('italy' in europe)
 
 # Add poland to europe
 europe["poland"]='warsaw'
 
 # Print europe
 print(europe)
+
+
+# The capital of Germany is not 'bonn'; it's 'berlin'. Update its value.
+# Australia is not in Europe, Austria is! Remove the key 'australia' from europe.
+# Print out europe to see if your cleaning work paid off.
+
+#solution:
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+europe["germany"]='berlin'
+
+# Remove australia
+del(europe['australia'])
+
+# Print europe
+print(europe)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
