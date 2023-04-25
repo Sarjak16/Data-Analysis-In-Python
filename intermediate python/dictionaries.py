8981175a752c3f9348fb76c2d64f798f3bea7f34
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-25 20:42:44 +0545

    Update dictionaries.py

--- intermediate python/dictionaries.py
+++ intermediate python/dictionaries.py
@@ -27,3 +27,19 @@ europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin','norway':'oslo'
 
 # Print europe
 print(europe)
+
+# Check out which keys are in europe by calling the keys() method on europe. Print out the result.
+# Print out the value that belongs to the key 'norway'.
+
+#solution
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
+
+
+
