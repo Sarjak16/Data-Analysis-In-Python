b99a764ed724c6a0b7fb386415a240d8b28a4ace
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-25 20:35:26 +0545

    Update dictionaries.py

--- intermediate python/dictionaries.py
+++ intermediate python/dictionaries.py
@@ -12,3 +12,18 @@ ind_ger=countries.index("germany")
 
 # Use ind_ger to print out capital of Germany
 print(capitals[ind_ger])
+
+# With the strings in countries and capitals, create a dictionary called europe with 4 key:value pairs. 
+#   Beware of capitalization! Make sure you use lowercase characters everywhere.
+# Print out europe to see if the result is what you expected.
+#solution:
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# From string in countries and capitals, create dictionary europe
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin','norway':'oslo' }
+
+# Print europe
+print(europe)
