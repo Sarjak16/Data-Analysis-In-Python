faf2251f8a915a5a89c031b896320e7b493feed0
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-26 11:30:31 +0545

    Update dictionary_to_dataframe.py

--- dictionary_to_dataframe.py
+++ dictionary_to_dataframe.py
@@ -116,7 +116,7 @@ import pandas as pd
 cars = pd.read_csv('cars.csv', index_col = 0)
 
 # Print out observation for Japan
-cars.loc[['JPN']]
+print(cars.loc['JPN'])
 
 # Print out observations for Australia and Egypt
-cars.loc[['AUS','EG']]
+print(cars.loc[['AUS','EG']])
