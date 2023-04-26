7df6f25d7c633d48d0ba5216c1f877661946993c
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-26 11:05:28 +0545

    Update dictionary_to_dataframe.py

--- dictionary_to_dataframe.py
+++ dictionary_to_dataframe.py
@@ -45,3 +45,17 @@ cars.index=row_labels
 
 # Print cars again
 print (cars)
+
+
+# To import CSV files you still need the pandas package: import it as pd.
+# Use pd.read_csv() to import cars.csv data as a DataFrame. Store this DataFrame as cars.
+# Print out cars. Does everything look OK?
+#solution:
+# Import pandas as pd
+import pandas as pd
+
+# Import the cars.csv data: cars
+cars= pd.read_csv('cars.csv')
+
+# Print out cars
+print(cars)
