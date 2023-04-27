108f71da7403e0d41aa7af9255495d8c8c3faae3
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-27 19:31:56 +0545

    Create statements.py

---
+++ intermediate python/statements.py
@@ -0,0 +1,16 @@
+# IF..................................................................................................................
+# Examine the if statement that prints out "looking around in the kitchen." if room equals "kit".
+# Write another if statement that prints out "big place!" if area is greater than 15.
+# solution:
+# Define variables
+room = "kit"
+area = 14.0
+
+# if statement for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+
+# if statement for area
+if area>15:
+    print("big place!")
+
