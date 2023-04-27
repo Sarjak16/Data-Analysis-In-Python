4e4ed9fa973fc2e0c1d6f6b25e0a1d6aac2ab643
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-27 19:52:53 +0545

    Update statements.py

--- intermediate python/statements.py
+++ intermediate python/statements.py
@@ -74,3 +74,14 @@ sel = cars [dr==True]
 
 # Print sel
 print(sel)
+
+# Convert the code to a one-liner that calculates the variable sel as before
+# solution:
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Convert code to a one-liner
+sel = cars[cars['drives_right']]
+# Print sel
+print(sel)
