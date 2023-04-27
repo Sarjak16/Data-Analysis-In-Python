b1c1fc7fcae869fdd3ea4d6492a170aac1ebc344
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-27 19:57:12 +0545

    Update statements.py

--- intermediate python/statements.py
+++ intermediate python/statements.py
@@ -33,6 +33,7 @@ if area > 15 :
 else :
     print("pretty small.")
    
+
 # elif....................................................................................
 # Add an elif to the second control structure such that "medium size, nice!" is printed out if area is greater than 10.
 #solution:
@@ -85,3 +86,19 @@ cars = pd.read_csv('cars.csv', index_col = 0)
 sel = cars[cars['drives_right']]
 # Print sel
 print(sel)
+
+# Select the cars_per_cap column from cars as a Pandas Series and store it as cpc.
+# Use cpc in combination with a comparison operator and 500. You want to end up with a boolean Series that's True if the corresponding country has a cars_per_cap of more than 500 and False otherwise. Store this boolean Series as many_cars.
+# Use many_cars to subset cars, similar to what you did before. Store the result as car_maniac.
+# Print out car_maniac to see if you got it right.
+#solution:
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Create car_maniac: observations that have a cars_per_cap over 500
+cpc = cars['cars_per_cap']
+many_cars = cpc > 500
+car_maniac = cars[many_cars]
+# Print car_maniac
+print(car_maniac)
