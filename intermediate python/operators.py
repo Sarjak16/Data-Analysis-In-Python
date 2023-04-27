2d3a27be532de952ec516c44affe25b2a2864c4e
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-27 19:03:09 +0545

    Update and rename equality.py to operators.py

--- intermediate python/operators.py
+++ intermediate python/operators.py
@@ -17,3 +17,19 @@ True==False
 # Compare a boolean with an integer
 True==1
 
+# grater and less.....................................................................................................
+# Write Python expressions, wrapped in a print() function, to check whether:
+# x is greater than or equal to -10. x has already been defined for you.
+# "test" is less than or equal to y. y has already been defined for you.
+# True is greater than False.
+#solution:
+# Comparison of integers
+x = -3 * 6
+print(x>=-10)
+
+# Comparison of strings
+y = "test"
+print("test"<=y)
+
+# Comparison of booleans
+print(True>False)
