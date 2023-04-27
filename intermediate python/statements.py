ce61b4cd7a711993e0bd9da8248724c6e0eb00df
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-27 19:37:49 +0545

    Update statements.py

--- intermediate python/statements.py
+++ intermediate python/statements.py
@@ -14,3 +14,24 @@ if room == "kit" :
 if area>15:
     print("big place!")
 
+#     ELSE........................................
+#     Add an else statement to the second control structure so that "pretty small." is printed out if area > 15 evaluates to False.
+#solution:
+# Define variables
+room = "kit"
+area = 14.0
+
+# if-else construct for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+else :
+    print("looking around elsewhere.")
+
+# if-else construct for area
+if area > 15 :
+    print("big place!")
+else :
+    print("pretty small.")
+
+
+
