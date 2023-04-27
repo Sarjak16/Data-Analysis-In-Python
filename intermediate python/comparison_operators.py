5e3e788a5e5609221408bcc047172e72bc536dff
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-27 19:18:41 +0545

    Update comparison_operators.py

--- intermediate python/comparison_operators.py
+++ intermediate python/comparison_operators.py
@@ -50,3 +50,22 @@ print(my_house>=18)
 
 # my_house less than your_house
 print(my_house< your_house)
+
+# Boolean...................................................................
+# Write Python expressions, wrapped in a print() function, to check whether:
+# my_kitchen is bigger than 10 and smaller than 18.
+# my_kitchen is smaller than 14 or bigger than 17.
+# double the area of my_kitchen is smaller than triple the area of your_kitchen.
+#solution:
+# Define variables
+my_kitchen = 18.0
+your_kitchen = 14.0
+
+# my_kitchen bigger than 10 and smaller than 18?
+print(my_kitchen>10 and my_kitchen<18)
+
+# my_kitchen smaller than 14 or bigger than 17?
+print(my_kitchen<14 or my_kitchen>17)
+
+# Double my_kitchen smaller than triple your_kitchen?
+print(2*my_kitchen or 3*your_kitchen)
