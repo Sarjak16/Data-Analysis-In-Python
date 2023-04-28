6ca7b995f0561c089a6ece4853740959a487d235
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-28 22:24:22 +0545

    Update Loop.py

--- intermediate python/Loop.py
+++ intermediate python/Loop.py
@@ -20,3 +20,13 @@ while x < 4 :
 #Next, decrease the value of offset by 1. You can do this with offset = offset - 1.
 #Finally, still within your loop, print out offset so you can see how it changes.
 
+# Initialize offset
+offset=8
+
+
+# Code the while loop
+while offset != 0:
+    print("correcting...")
+    offset = offset - 1
+    print(offset)
+
