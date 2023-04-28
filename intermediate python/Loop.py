ee0eb78530fdc6129840d84f4952f9ac645934b5
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-28 22:21:02 +0545

    Create Loop.py

---
+++ intermediate python/Loop.py
@@ -0,0 +1,22 @@
+#while loop...............................................................................................
+The while loop is like a repeated if statement. The code is executed over and over again, as long as the condition is True. Have another look at its recipe.
+
+while condition :
+    expression
+Can you tell how many printouts the following while loop will do?
+
+x = 1
+while x < 4 :
+    print(x)
+    x = x + 1
+    
+    answer:
+    3
+    
+#basic while loop
+#Create the variable offset with an initial value of 8.
+#Code a while loop that keeps running as long as offset is not equal to 0. Inside the while loop:
+#Print out the sentence "correcting...".
+#Next, decrease the value of offset by 1. You can do this with offset = offset - 1.
+#Finally, still within your loop, print out offset so you can see how it changes.
+
