478cd22d5209bbd0f89ecbb57ba566416bdba4bc
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-29 20:22:54 +0545

    Update Loop.py

--- intermediate python/Loop.py
+++ intermediate python/Loop.py
@@ -104,3 +104,28 @@ europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
 # Iterate over europe
 for key,value in europe.items() :
     print("the capital of " + key + "is" + str(value))
+
+    
+# Import the numpy package under the local alias np.
+# Write a for loop that iterates over all elements in np_height and prints out "x inches" for each element, where x is the value in the array.
+# Write a for loop that visits every element of the np_baseball array and prints it out.
+    
+#solution:
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for x in np_height :
+    print(str(x) + " inches")
+# For loop over np_baseball
+for x in np.nditer(np_baseball) :
+    print(x)
+
+    
+    
+    
+    
+    
+    
+    
+    
