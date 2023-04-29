2488e1395d61d41716ebc577600584a8ba6f91d7
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-29 19:13:50 +0545

    Update Loop.py

--- intermediate python/Loop.py
+++ intermediate python/Loop.py
@@ -80,3 +80,15 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 for index, area in enumerate(areas) :
     print("room " + str(index +1) + ": " + str(area))
 
+# Write a for loop that goes through each sublist of house and prints out the x is y sqm, where x is the name of the room and y is the area of the room.
+# solution
+# house list of lists
+house = [["hallway", 11.25], 
+         ["kitchen", 18.0], 
+         ["living room", 20.0], 
+         ["bedroom", 10.75], 
+         ["bathroom", 9.50]]
+         
+# Build a for loop from scratch
+for key, value in house:
+    print("the " + str(key) + " is " + str(value) + " sqm")
