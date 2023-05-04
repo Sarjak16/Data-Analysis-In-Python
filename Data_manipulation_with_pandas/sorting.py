10085757f0619fc371ec2ecea131fc6c5a01bcbf
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-04 20:15:02 +0545

    Update sorting.py

--- Data_manipulation_with_pandas/sorting.py
+++ Data_manipulation_with_pandas/sorting.py
@@ -26,3 +26,13 @@ homelessness_reg_fam = homelessness.sort_values("region", ascending= False)
 
 # Print the top few rows
 print(homelessness_reg_fam.head())
+
+
+
+
+
+# Sort homelessness by region, then descending family members
+homelessness_reg_fam = homelessness.sort_values(["region", "family_members"] ,ascending= [True, False])
+
+# Print the top few rows
+print(homelessness_reg_fam.head())
