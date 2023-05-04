74f31f4b77662af458c572e9eb05c0c9ff0a7595
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-04 20:11:40 +0545

    Update sorting.py

--- Data_manipulation_with_pandas/sorting.py
+++ Data_manipulation_with_pandas/sorting.py
@@ -7,3 +7,12 @@ homelessness_ind = homelessness.sort_values("individuals")
 
 # Print the top few rows
 print(homelessness_ind.head())
+
+
+# Sort homelessness by the number of homeless family_members in descending order, and save this as homelessness_fam.
+# Print the head of the sorted DataFrame.
+# Sort homelessness by descending family members
+homelessness_fam = homelessness.sort_values("family_members", ascending= False)
+
+# Print the top few rows
+print(homelessness_fam.head())
