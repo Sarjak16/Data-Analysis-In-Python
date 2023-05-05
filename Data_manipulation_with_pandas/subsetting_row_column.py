0034fd3019052a106fbe64c3188788056fc76f53
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-05 21:55:23 +0545

    Update subsetting_row_column.py

--- Data_manipulation_with_pandas/subsetting_row_column.py
+++ Data_manipulation_with_pandas/subsetting_row_column.py
@@ -15,3 +15,15 @@ canu = ["California", "Arizona", "Nevada", "Utah"]
 mojave_homelessness = mojave_homelessness = homelessness[homelessness['state'].isin(canu)]
 # See the result
 print(mojave_homelessness)
+
+# Add a new column to homelessness, named total, containing the sum of the individuals and family_members columns.
+# Add another column to homelessness, named p_individuals, containing the proportion of homeless people in each state who are individuals.
+
+# Add total col as sum of individuals and family_members
+homelessness['total'] = homelessness['individuals'] + homelessness['family_members']
+
+# Add p_individuals col as proportion of total that are individuals
+homelessness['p_individuals']= homelessness['individuals'] / homelessness['total']
+
+# See the result
+print(homelessness)
