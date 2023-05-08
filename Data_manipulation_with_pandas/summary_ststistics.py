089e9187d22c8c4c7d706e8b3b676eb2c404e5eb
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-08 20:36:45 +0545

    Update summary_ststistics.py

--- Data_manipulation_with_pandas/summary_ststistics.py
+++ Data_manipulation_with_pandas/summary_ststistics.py
@@ -40,6 +40,11 @@ def iqr(column):
 print(sales['temperature_c'].agg(iqr))
 
 # Update the column selection to use the custom iqr function with .agg() to print the IQR of temperature_c, fuel_price_usd_per_l, and unemployment, in that order.
+# A custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
 
+# Update to print IQR of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales[["temperature_c", 'fuel_price_usd_per_l', 'unemployment' ]].agg(iqr))
 
 # Update the aggregation functions called by .agg(): include iqr and np.median in that order.
