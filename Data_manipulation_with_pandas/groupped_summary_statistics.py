67dc5f184caa2add11d4bba7d804608a53d73061
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-09 09:57:36 +0545

    Update groupped_summary_statistics.py

--- Data_manipulation_with_pandas/groupped_summary_statistics.py
+++ Data_manipulation_with_pandas/groupped_summary_statistics.py
@@ -45,3 +45,12 @@ sales_by_type = sales.groupby("type")["weekly_sales"].sum()
 # Get proportion for each type
 sales_propn_by_type = sales_by_type/ sum(sales.weekly_sales)
 print(sales_propn_by_type)
+
+#Group sales by "type" and "is_holiday", take the sum of weekly_sales, and store as sales_by_type_is_holiday.
+
+# From previous step
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+
+# Group by type and is_holiday; calc total weekly sales
+sales_by_type_is_holiday = sales.groupby(["type", "is_holiday"])["weekly_sales"].sum()
+print(sales_by_type_is_holiday)
