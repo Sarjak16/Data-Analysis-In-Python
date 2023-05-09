48b8d475cdf7b8a1137e76bb34855ac0e2d32ed9
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-09 12:08:27 +0545

    Update pivot_tables.py

--- Data_manipulation_with_pandas/pivot_tables.py
+++ Data_manipulation_with_pandas/pivot_tables.py
@@ -20,5 +20,15 @@ print(mean_sales_by_type)
 
 # 2
 # Get the mean and median (using NumPy functions) of weekly_sales by type using .pivot_table() and store as mean_med_sales_by_type.
+
+# Import NumPy as np
+import numpy as np
+
+# Pivot for mean and median weekly_sales for each store type
+mean_med_sales_by_type = sales.pivot_table(values= "weekly_sales", index= "type", aggfunc= [np.mean, np.median])
+
+# Print mean_med_sales_by_type
+print(mean_med_sales_by_type)
+
 # 3
 # Get the mean of weekly_sales by type and is_holiday using .pivot_table() and store as mean_sales_by_type_holiday.
