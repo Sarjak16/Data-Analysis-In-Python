17cbc09c8e5d2fae9fb279cea6dd6d2b555a5cd8
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-09 09:54:31 +0545

    Update groupped_summary_statistics.py

--- Data_manipulation_with_pandas/groupped_summary_statistics.py
+++ Data_manipulation_with_pandas/groupped_summary_statistics.py
@@ -29,3 +29,19 @@ sales_C = sales[sales["type"]== "c"]["weekly_sales"].sum()
     # Get proportion for each type
     sales_propn_by_type = [sales_A, sales_B, sales_C] / sales_all
     print(sales_propn_by_type)
+
+    
+#     Calculations with .groupby()........................................................................................................................
+# The .groupby() method makes life much easier. In this exercise, you'll perform the same calculations as last time, except you'll use the .groupby() method.
+# You'll also perform calculations on data grouped by two variables to see if sales differ by store type depending on if it's a holiday week or not.
+
+# sales is available and pandas is loaded as pd.
+# Group sales by "type", take the sum of "weekly_sales", and store as sales_by_type.
+# Calculate the proportion of sales at each store type by dividing by the sum of sales_by_type. Assign to sales_propn_by_type.
+
+# Group by type; calc total weekly sales
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+
+# Get proportion for each type
+sales_propn_by_type = sales_by_type/ sum(sales.weekly_sales)
+print(sales_propn_by_type)
