ffa92d5bb9e502369f07b32d34d0ec0aaa45cf72
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-09 16:39:45 +0545

    Update explicit_indexes.py

--- Data_manipulation_with_pandas/explicit_indexes.py
+++ Data_manipulation_with_pandas/explicit_indexes.py
@@ -1,8 +1,10 @@
-Look at temperatures.
-Set the index of temperatures to "city", assigning to temperatures_ind.
-Look at temperatures_ind. How is it different from temperatures?
-Reset the index of temperatures_ind, keeping its contents.
-Reset the index of temperatures_ind, dropping its contents.
+#setting and removing indexes..................................................................................................................
+
+# Look at temperatures.
+# Set the index of temperatures to "city", assigning to temperatures_ind.
+# Look at temperatures_ind. How is it different from temperatures?
+# Reset the index of temperatures_ind, keeping its contents.
+# Reset the index of temperatures_ind, dropping its contents.
 
 
 # Look at temperatures
@@ -18,4 +20,24 @@ print(temperatures_ind.head())
 print(temperatures_ind.reset_index())
 
 # Reset the temperatures_ind index, dropping its contents
-print(temperatures_ind.reset_index(drop= True))
+# print(temperatures_ind.reset_index(drop= True))
+
+# Subsetting with .loc[]........................................................................................................................................
+# The killer feature for indexes is .loc[]: a subsetting method that accepts index values. When you pass it a single argument, it will take a subset of rows.
+
+# The code for subsetting using .loc[] can be easier to read than standard square bracket subsetting, which can make your code less burdensome to maintain.
+
+# pandas is loaded as pd. temperatures and temperatures_ind are available; the latter is indexed by city.
+
+# Create a list called cities that contains "Moscow" and "Saint Petersburg".
+# Use [] subsetting to filter temperatures for rows where the city column takes a value in the cities list.
+# Use .loc[] subsetting to filter temperatures_ind for rows where the city is in the cities list.
+
+# Make a list of cities to subset on
+cities = ["Moscow", "Saint Petersburg"]
+
+# Subset temperatures using square brackets
+print(temperatures[temperatures["city"].isin(cities)])
+
+# Subset temperatures_ind using .loc[]
+print(temperatures_ind.loc[cities])
