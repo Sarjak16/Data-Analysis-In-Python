a109ac6801b786ad2f10c2b53faeb7093236be47
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-09 12:06:19 +0545

    Create pivot_tables.py

---
+++ Data_manipulation_with_pandas/pivot_tables.py
@@ -0,0 +1,24 @@
+#Pivoting on one variable...............................................................................................................................
+# Pivot tables are the standard way of aggregating data in spreadsheets.
+
+# In pandas, pivot tables are essentially another way of performing grouped calculations. That is, the .pivot_table() method is an alternative to .groupby().
+
+# In this exercise, you'll perform calculations using .pivot_table() to replicate the calculations you performed in the last lesson using .groupby().
+
+# sales is available and pandas is imported as pd.
+
+
+
+# 1
+# Get the mean weekly_sales by type using .pivot_table() and store as mean_sales_by_type.
+# Pivot for mean weekly_sales for each store type
+mean_sales_by_type = sales.pivot_table(values= "weekly_sales", index= "type")
+
+# Print mean_sales_by_type
+print(mean_sales_by_type)
+
+
+# 2
+# Get the mean and median (using NumPy functions) of weekly_sales by type using .pivot_table() and store as mean_med_sales_by_type.
+# 3
+# Get the mean of weekly_sales by type and is_holiday using .pivot_table() and store as mean_sales_by_type_holiday.
