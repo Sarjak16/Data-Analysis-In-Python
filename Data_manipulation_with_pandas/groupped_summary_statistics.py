7125227d10d115e2db61f1a326021bafdf5cb1c4
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-09 11:48:52 +0545

    Update groupped_summary_statistics.py

--- Data_manipulation_with_pandas/groupped_summary_statistics.py
+++ Data_manipulation_with_pandas/groupped_summary_statistics.py
@@ -54,3 +54,31 @@ sales_by_type = sales.groupby("type")["weekly_sales"].sum()
 # Group by type and is_holiday; calc total weekly sales
 sales_by_type_is_holiday = sales.groupby(["type", "is_holiday"])["weekly_sales"].sum()
 print(sales_by_type_is_holiday)
+
+
+# Multiple grouped summaries...................................................................................................
+# Earlier in this chapter, you saw that the .agg() method is useful to compute multiple statistics on multiple variables. 
+# It also works with grouped data. NumPy, which is imported as np, has many different summary statistics functions, including: np.min, np.max, np.mean, and np.median.
+
+# sales is available and pandas is imported as pd.
+
+# Import numpy with the alias np.
+# Get the min, max, mean, and median of weekly_sales for each store type using .groupby() and .agg(). Store this as sales_stats. Make sure to use numpy functions!
+# Get the min, max, mean, and median of unemployment and fuel_price_usd_per_l for each store type. Store this as unemp_fuel_stats.
+
+# Import numpy with the alias np
+import numpy as np
+
+# For each store type, aggregate weekly_sales: get min, max, mean, and median
+sales_stats = sales.groupby("type")["weekly_sales"].agg([np.min, np.max, np.mean, np.median])
+
+# Print sales_stats
+print(sales_stats)
+
+# For each store type, aggregate unemployment and fuel_price_usd_per_l: get min, max, mean, and median
+unemp_fuel_stats = sales.groupby("type")[["unemployment", "fuel_price_usd_per_l"]].agg([np.min, np.max, np.mean, np.median])
+
+# Print unemp_fuel_stats
+print(unemp_fuel_stats)
+
+
