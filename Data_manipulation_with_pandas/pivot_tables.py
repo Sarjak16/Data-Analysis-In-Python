3aa5486517c264b69fad42925304bf2098b09d5a
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-09 12:11:54 +0545

    Update pivot_tables.py

--- Data_manipulation_with_pandas/pivot_tables.py
+++ Data_manipulation_with_pandas/pivot_tables.py
@@ -32,3 +32,8 @@ print(mean_med_sales_by_type)
 
 # 3
 # Get the mean of weekly_sales by type and is_holiday using .pivot_table() and store as mean_sales_by_type_holiday.
+# Pivot for mean weekly_sales by store type and holiday 
+mean_sales_by_type_holiday = sales.pivot_table(values="weekly_sales", index= "type", columns= "is_holiday")
+
+# Print mean_sales_by_type_holiday
+print(mean_sales_by_type_holiday)
