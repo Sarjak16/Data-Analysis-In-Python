f5a2e803ef3077b9c17c950a91f5f4d464ef02b2
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-10 20:38:02 +0545

    Update working_with_table.py

--- Data_manipulation_with_pandas/working_with_table.py
+++ Data_manipulation_with_pandas/working_with_table.py
@@ -24,3 +24,22 @@ temp_by_country_city_vs_year.loc[('Egypt','Cairo'):('India','Delhi')]
 
 # Subset for Egypt, Cairo to India, Delhi, and 2005 to 2010
 temp_by_country_city_vs_year.loc[('Egypt','Cairo'):('India','Delhi'),'2005':'2010']
+
+
+
+# Calculate the mean temperature for each year, assigning to mean_temp_by_year.
+# Filter mean_temp_by_year for the year that had the highest mean temperature.
+# Calculate the mean temperature for each city (across columns), assigning to mean_temp_by_city.
+# Filter mean_temp_by_city for the city that had the lowest mean temperature.
+
+# Get the worldwide mean temp by year
+mean_temp_by_year = temp_by_country_city_vs_year.mean()
+
+# Filter for the year that had the highest mean temp
+print(mean_temp_by_year[mean_temp_by_year==mean_temp_by_year.max()])
+
+# Get the mean temp by city
+mean_temp_by_city = temp_by_country_city_vs_year.mean(axis="columns")
+
+# Filter for the city that had the lowest mean temp
+print(mean_temp_by_city[mean_temp_by_city==mean_temp_by_city.min()])
