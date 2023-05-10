005c157cb2cab4d8bb5ffff183bae61a22d5f008
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-10 20:34:35 +0545

    Update working_with_table.py

--- Data_manipulation_with_pandas/working_with_table.py
+++ Data_manipulation_with_pandas/working_with_table.py
@@ -9,3 +9,18 @@ temp_by_country_city_vs_year = temperatures.pivot_table('avg_temp_c', index=['co
 
 # See the result
 print(temp_by_country_city_vs_year)
+
+
+# Use .loc[] on temp_by_country_city_vs_year to take subsets.
+
+# From Egypt to India.
+# From Egypt, Cairo to India, Delhi.
+# From Egypt, Cairo to India, Delhi, and 2005 to 2010.
+# Subset for Egypt to India
+temp_by_country_city_vs_year.loc['Egypt':'India']
+
+# Subset for Egypt, Cairo to India, Delhi
+temp_by_country_city_vs_year.loc[('Egypt','Cairo'):('India','Delhi')]
+
+# Subset for Egypt, Cairo to India, Delhi, and 2005 to 2010
+temp_by_country_city_vs_year.loc[('Egypt','Cairo'):('India','Delhi'),'2005':'2010']
