1f4d38511f73ae951674df6f4d9676d14b3f6c74
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-11 21:36:11 +0545

    Create reading&writing_csv.py

---
+++ Data_manipulation_with_pandas/reading&writing_csv.py
@@ -0,0 +1,8 @@
+# Read the CSV file "airline_bumping.csv" and store it as a DataFrame called airline_bumping.
+# Print the first few rows of airline_bumping.
+
+# Read CSV as DataFrame called airline_bumping
+airline_bumping = pd.read_csv("airline_bumping.csv")
+
+# Take a look at the DataFrame
+print(airline_bumping.head())
