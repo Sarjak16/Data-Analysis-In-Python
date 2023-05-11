6eadf50d1f7d8ec7310e2e1e3b2ccda700a0c7d8
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-11 21:38:20 +0545

    Update reading&writing_csv.py

--- Data_manipulation_with_pandas/reading&writing_csv.py
+++ Data_manipulation_with_pandas/reading&writing_csv.py
@@ -6,3 +6,12 @@ airline_bumping = pd.read_csv("airline_bumping.csv")
 
 # Take a look at the DataFrame
 print(airline_bumping.head())
+
+
+#For each airline group, select the nb_bumped, and total_passengers columns, and calculate the sum (for both years). Store this as airline_totals.
+# From previous step
+airline_bumping = pd.read_csv("airline_bumping.csv")
+print(airline_bumping.head())
+
+# For each airline, select nb_bumped and total_passengers and sum
+airline_totals = airline_bumping.groupby("airline")[["nb_bumped", "total_passengers"]].sum()
