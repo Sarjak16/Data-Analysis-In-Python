79695f053c0251ddca2ef75a7404613eaf34790d
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-11 21:03:01 +0545

    Update visualizing_data.py

--- Data_manipulation_with_pandas/visualizing_data.py
+++ Data_manipulation_with_pandas/visualizing_data.py
@@ -63,3 +63,15 @@ plt.legend("conventional", "organic")
 
 # Show the plot
 plt.show()
+
+# # Histogram of conventional avg_price 
+avocados[avocados["type"] == "conventional"]["avg_price"].hist(bins=20, alpha=0.5)
+
+# Histogram of organic avg_price
+avocados[avocados["type"] == "organic"]["avg_price"].hist(bins=20, alpha=0.5)
+
+# Add a legend
+plt.legend(["conventional", "organic"])
+
+# Show the plot
+plt.show()
