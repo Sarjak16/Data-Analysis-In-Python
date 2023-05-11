3fb8d48b99e26b44d0389c6ef08dd65439956eb5
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-11 21:43:43 +0545

    Update reading&writing_csv.py

--- Data_manipulation_with_pandas/reading&writing_csv.py
+++ Data_manipulation_with_pandas/reading&writing_csv.py
@@ -27,3 +27,12 @@ airline_totals = airline_bumping.groupby("airline")[["nb_bumped", "total_passeng
 # Create new col, bumps_per_10k: no. of bumps per 10k passengers for each airline
 airline_totals["bumps_per_10k"] = airline_totals["nb_bumped"] / airline_totals["total_passengers"]  * 10000
 
+# Print airline_totals to see the results of your manipulations.
+# From previous steps
+airline_bumping = pd.read_csv("airline_bumping.csv")
+print(airline_bumping.head())
+airline_totals = airline_bumping.groupby("airline")[["nb_bumped", "total_passengers"]].sum()
+airline_totals["bumps_per_10k"] = airline_totals["nb_bumped"] / airline_totals["total_passengers"] * 10000
+
+# Print airline_totals
+print(airline_totals)
