d63b01586f3513a48e0e4e2cab9de8603f28e0f4
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-11 21:18:47 +0545

    Update missing_values.py

--- Data_manipulation_with_pandas/missing_values.py
+++ Data_manipulation_with_pandas/missing_values.py
@@ -41,3 +41,22 @@ avocados_2016[cols_with_missing].hist()
 
 # Show the plot
 plt.show()
+
+
+# Replace the missing values of avocados_2016 with 0s and store the result as avocados_filled.
+# Create a histogram of the cols_with_missing columns of avocados_filled.
+
+
+# From previous step
+cols_with_missing = ["small_sold", "large_sold", "xl_sold"]
+avocados_2016[cols_with_missing].hist()
+plt.show()
+
+# Fill in missing values with 0
+avocados_filled = avocados_2016.fillna(0)
+
+# Create histograms of the filled columns
+avocados_filled[cols_with_missing].hist()
+
+# Show the plot
+plt.show()
