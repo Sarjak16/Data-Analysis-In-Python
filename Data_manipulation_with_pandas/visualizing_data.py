05c7d601982a21922a745c78b61ab6cabc668b39
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-11 20:41:29 +0545

    Update visualizing_data.py

--- Data_manipulation_with_pandas/visualizing_data.py
+++ Data_manipulation_with_pandas/visualizing_data.py
@@ -17,3 +17,21 @@ nb_sold_by_size.plot(kind="bar")
 
 # Show the plot
 plt.show()
+
+
+# Get the total number of avocados sold on each date. The DataFrame has two rows for each date—one for organic, and one for conventional.
+# Save this as nb_sold_by_date.
+# Create a line plot of the number of avocados sold.
+# Show the plot.
+
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+
+# Get the total number of avocados sold on each date
+nb_sold_by_date = avocados.groupby("date")["nb_sold"].sum()
+
+# Create a line plot of the number of avocados sold by date
+nb_sold_by_date.plot(kind="line")
+
+# Show the plot
+plt.show()
