a0bc1304daa84964ae85bf0b07406df2fa949f16
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-11 21:13:25 +0545

    Update missing_values.py

--- Data_manipulation_with_pandas/missing_values.py
+++ Data_manipulation_with_pandas/missing_values.py
@@ -17,3 +17,13 @@ avocados_2016.isna().sum().plot(kind="bar")
 
 # Show plot
 plt.show()
+
+
+# Remove the rows of avocados_2016 that contain missing values and store the remaining rows in avocados_complete.
+# Verify that all missing values have been removed from avocados_complete. Calculate each column that has NAs and print.
+
+# Remove rows with missing values
+avocados_complete = avocados_2016.dropna()
+
+# Check if any columns contain missing values
+print(avocados_complete.isna().any())
