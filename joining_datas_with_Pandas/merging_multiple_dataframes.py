ba88d10df49bc78cf786c64be369b5deedbbc233
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-14 12:35:26 +0545

    Update merging_multiple_dataframes.py

--- joining_datas_with_Pandas/merging_multiple_dataframes.py
+++ joining_datas_with_Pandas/merging_multiple_dataframes.py
@@ -58,3 +58,21 @@ pop_vac_lic = land_cen_lic.groupby(['ward', 'pop_2010', 'vacant'],
 # Merge land_use and census and merge result with licenses including suffixes
 land_cen_lic = land_use.merge(census, on='ward')\
 .merge(licenses, on='ward', suffixes=('_cen', '_lic'))
+
+
+# Sort pop_vac_lic by vacant, account, andpop_2010 in descending, ascending, and ascending order respectively. Save it as sorted_pop_vac_lic.
+
+# Merge land_use and census and merge result with licenses including suffixes
+land_cen_lic = land_use.merge(census, on='ward') \
+                    .merge(licenses, on='ward', suffixes=('_cen','_lic'))
+
+# Group by ward, pop_2010, and vacant, then count the # of accounts
+pop_vac_lic = land_cen_lic.groupby(['ward','pop_2010','vacant'], 
+                                   as_index=False).agg({'account':'count'})
+
+# Sort pop_vac_lic and print the results
+sorted_pop_vac_lic = pop_vac_lic.sort_values(['vacant', 'account', 'pop_2010'],
+                                             ascending=[ False, True, True])
+
+# Print the top few rows of sorted_pop_vac_lic
+print(sorted_pop_vac_lic.head())
