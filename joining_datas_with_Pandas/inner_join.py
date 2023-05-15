49eb4bec5513caa6b270e09a86ee104a50a1a46a
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-15 22:23:42 +0545

    Update inner_join.py

--- joining_datas_with_Pandas/inner_join.py
+++ joining_datas_with_Pandas/inner_join.py
@@ -87,3 +87,13 @@ toystory_tag = toy_story.merge(taglines, on='id', how='left')
 # Print the rows and shape of toystory_tag
 print(toystory_tag)
 print(toystory_tag.shape)
+
+
+# With toy_story as the left table, merge to it taglines on the id column with an inner join, and save as toystory_tag.
+
+# Merge the toy_story and taglines tables with a inner join
+toystory_tag = toy_story.merge(taglines, on='id')
+
+# Print the rows and shape of toystory_tag
+print(toystory_tag)
+print(toystory_tag.shape)
