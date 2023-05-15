3b6915558f24bc47579e0b147a2bb3db19ed53e5
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-15 22:37:38 +0545

    Update inner_join.py

--- joining_datas_with_Pandas/inner_join.py
+++ joining_datas_with_Pandas/inner_join.py
@@ -113,3 +113,13 @@ action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right',
 
 # Print the first few rows of action_scifi to see the structure
 print(action_scifi.head())
+
+
+
+# From action_scifi, subset only the rows where the genre_act column is null.
+# Merge action_movies to the scifi_movies with right join
+action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right',
+                                   suffixes=('_act','_sci'))
+
+# From action_scifi, select only the rows where the genre_act column is null
+scifi_only = action_scifi[action_scifi['genre_act'].isnull()]
