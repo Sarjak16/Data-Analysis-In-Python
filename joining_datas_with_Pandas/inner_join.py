92427d54e18638a86d8883219456bc1417dfdd24
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-15 22:31:23 +0545

    Update inner_join.py

--- joining_datas_with_Pandas/inner_join.py
+++ joining_datas_with_Pandas/inner_join.py
@@ -97,3 +97,10 @@ toystory_tag = toy_story.merge(taglines, on='id')
 # Print the rows and shape of toystory_tag
 print(toystory_tag)
 print(toystory_tag.shape)
+
+
+# Right join..........................................................................................................................................
+
+# Merge action_movies and scifi_movies tables with a right join on movie_id. Save the result as action_scifi.
+# Merge action_movies to scifi_movies with right join
+action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right')
