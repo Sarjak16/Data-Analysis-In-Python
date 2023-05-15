f1209bcacd4f7164d6100ac064e144092b07b9a3
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-15 22:43:18 +0545

    Update inner_join.py

--- joining_datas_with_Pandas/inner_join.py
+++ joining_datas_with_Pandas/inner_join.py
@@ -140,3 +140,21 @@ movies_and_scifi_only = movies.merge(scifi_only, left_on='id', right_on='movie_i
 # Print the first few rows and shape of movies_and_scifi_only
 print(movies_and_scifi_only.head())
 print(movies_and_scifi_only.shape)
+
+
+
+# Merge movie_to_genres and pop_movies using a right join. Save the results as genres_movies.
+# Group genres_movies by genre and count the number of id values.
+
+
+# Use right join to merge the movie_to_genres and pop_movies tables
+genres_movies = movie_to_genres.merge(pop_movies, how='right', 
+                                      right_on='id', 
+                                      left_on='movie_id')
+
+# Count the number of genres
+genre_count = genres_movies.groupby('genre').agg({'id':'count'})
+
+# Plot a bar chart of the genre_count
+genre_count.plot(kind='bar')
+plt.show()
