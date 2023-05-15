04a374cf10082d5c3829c425fb7731c73c8e7e51
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-15 22:40:08 +0545

    Update inner_join.py

--- joining_datas_with_Pandas/inner_join.py
+++ joining_datas_with_Pandas/inner_join.py
@@ -123,3 +123,20 @@ action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right',
 
 # From action_scifi, select only the rows where the genre_act column is null
 scifi_only = action_scifi[action_scifi['genre_act'].isnull()]
+
+
+# Merge movies and scifi_only using the id column in the left table and the movie_id column in the right table with an inner join.
+
+# Merge action_movies to the scifi_movies with right join
+action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right',
+                                   suffixes=('_act','_sci'))
+
+# From action_scifi, select only the rows where the genre_act column is null
+scifi_only = action_scifi[action_scifi['genre_act'].isnull()]
+
+# Merge the movies and scifi_only tables with an inner join
+movies_and_scifi_only = movies.merge(scifi_only, left_on='id', right_on='movie_id')
+
+# Print the first few rows and shape of movies_and_scifi_only
+print(movies_and_scifi_only.head())
+print(movies_and_scifi_only.shape)
