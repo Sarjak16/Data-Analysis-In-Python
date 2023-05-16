926906b0672d87282a9b180387a27314a147bb70
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-16 19:57:14 +0545

    Create filtering_joins.py

---
+++ joining_datas_with_Pandas/filtering_joins.py
@@ -0,0 +1,26 @@
+# anti join...................................................................................................................................
+
+# Merge employees and top_cust with a left join, setting indicator argument to True. Save the result to empl_cust.
+# Merge employees and top_cust
+empl_cust = employees.merge(top_cust, on='empl_cust', 
+                            how='left', indicator= True)
+
+# Select the srid column of empl_cust and the rows where _merge is 'left_only'. Save the result to srid_list.
+# Merge employees and top_cust
+empl_cust = employees.merge(top_cust, on='srid', 
+                            how='left', indicator=True)
+
+# Select the srid column where _merge is left_only
+srid_list = empl_cust.loc[empl_cust['_merge'] =='left_only' 'srid']
+
+# Subset the employees table and select those rows where the srid is in the variable srid_list and print the results.
+
+# Merge employees and top_cust
+empl_cust = employees.merge(top_cust, on='srid', 
+                                 how='left', indicator=True)
+
+# Select the srid column where _merge is left_only
+srid_list = empl_cust.loc[empl_cust['_merge'] == 'left_only', 'srid']
+
+# Get employees not working with top customers
+print(employees[employees['srid'].isin(srid_list)])
