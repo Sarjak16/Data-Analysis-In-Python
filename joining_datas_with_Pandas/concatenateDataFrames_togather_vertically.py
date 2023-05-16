8d821c409a0b5c19e1371fbb70e79b7436c65da9
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-16 20:18:54 +0545

    Create concatenateDataFrames_togather_vertically.py

---
+++ joining_datas_with_Pandas/concatenateDataFrames_togather_vertically.py
@@ -0,0 +1,23 @@
+#concatenation basics..........................................................................................................
+# Concatenate tracks_master, tracks_ride, and tracks_st, in that order, setting sort to True.
+
+
+# Concatenate tracks_master, tracks_ride, and tracks_st, where the index goes from 0 to n-1.
+
+# Concatenate tracks_master, tracks_ride, and tracks_st, showing only columns that are in all tables.
+
+# Concatenate the tracks
+tracks_from_albums = pd.concat([tracks_master, tracks_ride, tracks_st],sort=True)
+print(tracks_from_albums)
+
+# Concatenate the tracks so the index goes from 0 to n-1
+tracks_from_albums = pd.concat([tracks_master,
+                               tracks_ride, tracks_st], ignore_index= True,
+                               sort=True)
+print(tracks_from_albums)
+
+# Concatenate the tracks, show only columns names that are in all tables
+tracks_from_albums = pd.concat([tracks_master,
+                               tracks_ride, tracks_st], join='inner',
+                               sort=True)
+print(tracks_from_albums)
