f7d0f35e166ba0905d58fe7a55323661cf5a094f
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-16 19:35:03 +0545

    Update merging_on_indexes.py

--- joining_datas_with_Pandas/merging_on_indexes.py
+++ joining_datas_with_Pandas/merging_on_indexes.py
@@ -11,3 +11,20 @@ movies_ratings = movies.merge(ratings, on='id', how='left')
 
 # Print the first few rows of movies_ratings
 print(movies_ratings.head())
+
+
+# With the sequels table on the left, merge to it the financials table on index named id,
+# ensuring that all the rows from the sequels are returned and some rows from the other table may not be returned, Save the results to sequels_fin.
+# Merge sequels and financials on index id
+sequels_fin = sequels.merge(financials, on='id', how='left')
+
+# Merge the sequels_fin table to itself with an inner join,
+# where the left and right tables merge on sequel and id respectively with suffixes equal to ('_org','_seq'), saving to orig_seq.
+# Merge sequels and financials on index id
+sequels_fin = sequels.merge(financials, on='id', how='left')
+
+# Self merge with suffixes as inner join with left on sequel and right on id
+orig_seq = sequels_fin.merge(sequels_fin, how='inner', left_on='sequel', right_on='id', right_index=True, suffixes=('_org','_seq'))
+
+# Add calculation to subtract revenue_org from revenue_seq 
+orig_seq['diff'] = orig_seq['revenue_seq'] - orig_seq['revenue_org']
