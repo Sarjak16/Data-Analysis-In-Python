d35bd1652f72696f34a089a4ad5cd55cc9fe01fd
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-17 18:07:28 +0545

    Update merge_asof().py

--- joining_datas_with_Pandas/merge_asof().py
+++ joining_datas_with_Pandas/merge_asof().py
@@ -29,3 +29,43 @@ is_recession = ['r' if s=='recession' else 'g' for s in gdp_recession['econ_stat
 # Plot a bar chart of gdp_recession
 gdp_recession.plot(kind='bar', y='gdp', x='date', color=is_recession, rot=90)
 plt.show()
+
+# Use merge_ordered() on gdp and pop on columns country and date with the fill feature, save to gdp_pop and print.
+# Merge gdp and pop on date and country with fill
+gdp_pop = pd.merge_ordered(gdp, pop, on=['country', 'date'], fill_method='ffill')
+
+# Add a column named gdp_per_capita to gdp_pop that divides gdp by pop.
+# Merge gdp and pop on date and country with fill
+gdp_pop = pd.merge_ordered(gdp, pop, on=['country','date'], fill_method='ffill')
+
+# Add a column named gdp_per_capita to gdp_pop that divides the gdp by pop
+gdp_pop['gdp_per_capita'] = gdp_pop['gdp'] / gdp_pop['pop']
+
+# Pivot gdp_pop so values='gdp_per_capita', index='date', and columns='country', save as gdp_pivot.
+# Merge gdp and pop on date and country with fill
+gdp_pop = pd.merge_ordered(gdp, pop, on=['country','date'], fill_method='ffill')
+
+# Add a column named gdp_per_capita to gdp_pop that divides the gdp by pop
+gdp_pop['gdp_per_capita'] = gdp_pop['gdp'] / gdp_pop['pop']
+
+# Pivot table of gdp_per_capita, where index is date and columns is country
+gdp_pivot = gdp_pop.pivot_table('gdp_per_capita', 'date', 'country')
+
+# Use .query() to select rows from gdp_pivot where date is greater than equal to "1991-01-01". Save as recent_gdp_pop.
+
+# Merge gdp and pop on date and country with fill
+gdp_pop = pd.merge_ordered(gdp, pop, on=['country','date'], fill_method='ffill')
+
+# Add a column named gdp_per_capita to gdp_pop that divides the gdp by pop
+gdp_pop['gdp_per_capita'] = gdp_pop['gdp'] / gdp_pop['pop']
+
+# Pivot data so gdp_per_capita, where index is date and columns is country
+gdp_pivot = gdp_pop.pivot_table('gdp_per_capita', 'date', 'country')
+
+# Select dates equal to or greater than 1991-01-01
+recent_gdp_pop = gdp_pivot.query('date>="1991-01-01"')
+
+# Plot recent_gdp_pop
+recent_gdp_pop.plot(rot=90)
+plt.show()
+
