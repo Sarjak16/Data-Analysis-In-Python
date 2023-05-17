9ae91e7fc3f8cb6236379d490174917a0d37c307
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-17 18:45:46 +0545

    Update merge_asof().py

--- joining_datas_with_Pandas/merge_asof().py
+++ joining_datas_with_Pandas/merge_asof().py
@@ -69,3 +69,25 @@ recent_gdp_pop = gdp_pivot.query('date>="1991-01-01"')
 recent_gdp_pop.plot(rot=90)
 plt.show()
 
+# Use .melt() to unpivot all of the columns of ur_wide except year and ensure that the columns with the months and values are named month and unempl_rate, respectively. Save the result as ur_tall.
+# Add a column to ur_tall named date which combines the year and month columns as year-month format into a larger string, and converts it to a date data type.
+# Sort ur_tall by date and save as ur_sorted.
+# Using ur_sorted, plot unempl_rate on the y-axis and date on the x-axis.
+
+# unpivot everything besides the year column
+ur_tall =  ur_wide.melt(id_vars=['year'], var_name='month', 
+                       value_name='unempl_rate')
+
+
+# Create a date column using the month and year columns of ur_tall
+ur_tall['date'] = pd.to_datetime(ur_tall['month'] + '-' + ur_tall['year'])
+
+# Sort ur_tall by date in ascending order
+
+ur_sorted = ur_tall.sort_values('date')
+
+
+
+# Plot the unempl_rate by date
+ur_sorted.plot(x='date', y='unempl_rate')
+plt.show()
