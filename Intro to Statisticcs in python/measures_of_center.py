a468f32fd7001ecf122a54ccf2c73b1de36fe80c
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-18 23:14:17 +0545

    Update measures_of_center.py

--- Intro to Statisticcs in python/measures_of_center.py
+++ Intro to Statisticcs in python/measures_of_center.py
@@ -1,7 +1,7 @@
-Mean and median....................................................................................................................
-Import numpy with the alias np.
-Create two DataFrames: one that holds the rows of food_consumption for 'Belgium' and another that holds rows for 'USA'. Call these be_consumption and usa_consumption.
-Calculate the mean and median of kilograms of food consumed per person per year for both countries.
+# Mean and median....................................................................................................................
+# Import numpy with the alias np.
+# Create two DataFrames: one that holds the rows of food_consumption for 'Belgium' and another that holds rows for 'USA'. Call these be_consumption and usa_consumption.
+# Calculate the mean and median of kilograms of food consumed per person per year for both countries.
 
 # Import numpy with alias np
 import numpy as np
@@ -19,3 +19,16 @@ print(np.median(be_consumption['consumption']))
 # Calculate mean and median consumption in USA
 print(np.mean(usa_consumption['consumption']))
 print(np.median(usa_consumption['consumption']))
+
+# Subset food_consumption for rows with data about Belgium and the USA.
+# Group the subsetted data by country and select only the consumption column.
+# Calculate the mean and median of the kilograms of food consumed per person per year in each country using .agg().
+
+# Import numpy as np
+import numpy as np
+
+# Subset for Belgium and USA only
+be_and_usa = food_consumption[(food_consumption['country']== 'Belgium') | (food_consumption['country']== 'USA')]
+
+# Group by country, select consumption column, and compute mean and median
+print(be_and_usa.groupby('country')['consumption'].agg([np.mean, np.median]))
