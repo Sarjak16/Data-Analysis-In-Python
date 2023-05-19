3a300cd0371e521d09e2df676fbc64131d0ee62a
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-19 13:10:52 +0545

    Update measuring_chances.py

--- Intro to Statisticcs in python/measuring_chances.py
+++ Intro to Statisticcs in python/measuring_chances.py
@@ -11,3 +11,13 @@ counts = amir_deals['product'].value_counts()
 # Calculate probability of picking a deal with each product
 probs = counts / len(amir_deals['product'])
 print(probs)
+
+# sampling deals.........................................................................................................\
+# Set the random seed to 24.
+# Take a sample of 5 deals without replacement and store them as sample_without_replacement.
+# Set random seed
+np.random.seed(24)
+
+# Sample 5 deals without replacement
+sample_without_replacement = amir_deals.sample(5, replace=False)
+print(sample_without_replacement)
