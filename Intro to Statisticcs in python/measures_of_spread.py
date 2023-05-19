b1d347c2b8ca47650c98991cc825d7337aea6725
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-19 12:20:49 +0545

    Update measures_of_spread.py

--- Intro to Statisticcs in python/measures_of_spread.py
+++ Intro to Statisticcs in python/measures_of_spread.py
@@ -13,3 +13,26 @@ print(np.quantile(food_consumption['co2_emission'],np.linspace(0, 1, 6)))
 # Calculate the eleven quantiles of co2_emission that split up the data into ten pieces (deciles).
 # Calculate the deciles of co2_emission
 print(np.quantile(food_consumption['co2_emission'],np.linspace(0, 1, 11)))
+
+
+# varience na std
+# Calculate the variance and standard deviation of co2_emission for each food_category by grouping and aggregating.
+# Import matplotlib.pyplot with alias plt.
+# Create a histogram of co2_emission for the beef food_category and show the plot.
+# Create a histogram of co2_emission for the eggs food_category and show the plot.
+
+# Print variance and sd of co2_emission for each food_category
+print(food_consumption.groupby('food_category')['co2_emission'].agg([np.var, np.std]))
+
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+
+# Create histogram of co2_emission for food_category 'beef'
+food_consumption[food_consumption['food_category'] == 'beef']['co2_emission'].hist()
+# Show plot
+plt.show()
+
+# Create histogram of co2_emission for food_category 'eggs'
+food_consumption[food_consumption['food_category'] == 'eggs']['co2_emission'].hist();
+# Show plot
+plt.show()
