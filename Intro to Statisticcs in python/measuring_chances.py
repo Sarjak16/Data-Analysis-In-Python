9ef0eff7cd794a41ac511f881998dab6fb51a51a
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-19 13:12:49 +0545

    Update measuring_chances.py

--- Intro to Statisticcs in python/measuring_chances.py
+++ Intro to Statisticcs in python/measuring_chances.py
@@ -21,3 +21,13 @@ np.random.seed(24)
 # Sample 5 deals without replacement
 sample_without_replacement = amir_deals.sample(5, replace=False)
 print(sample_without_replacement)
+
+# Take a sample of 5 deals with replacement and save as sample_with_replacement.
+# Set random seed
+np.random.seed(24)
+
+# Sample 5 deals with replacement
+sample_with_replacement = amir_deals.sample(5, replace= True)
+print(sample_with_replacement)
+
+
