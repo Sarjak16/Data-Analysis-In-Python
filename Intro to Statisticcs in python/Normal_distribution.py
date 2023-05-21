7aff5e218bbd9f3ff8dc630750bdb64d87067c45
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-21 16:41:24 +0545

    Update Normal_distribution.py

--- Intro to Statisticcs in python/Normal_distribution.py
+++ Intro to Statisticcs in python/Normal_distribution.py
@@ -30,3 +30,24 @@ print(prob_3000_to_7000)
 pct_25 = norm.ppf(0.25, 5000, 2000)
 
 print(pct_25)
+
+
+
+# .............................................................................................................................
+# Currently, Amir's average sale amount is $5000. Calculate what his new average amount will be if it increases by 20% and store this in new_mean.
+# Amir's current standard deviation is $2000. Calculate what his new standard deviation will be if it increases by 30% and store this in new_sd.
+# Create a variable called new_sales, which contains 36 simulated amounts from a normal distribution with a mean of new_mean and a standard deviation of new_sd.
+# Plot the distribution of the new_sales amounts using a histogram and show the plot.
+
+# Calculate new average amount
+new_mean = 5000 * 1.2
+
+# Calculate new standard deviation
+new_sd = 2000 * 1.3
+
+# Simulate 36 new sales
+new_sales = norm.rvs(new_mean, new_sd, size=36)
+
+# Create histogram and show
+plt.hist(new_sales)
+plt.show()
