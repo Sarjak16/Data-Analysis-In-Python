068c956e84bf345bc8f511317020f4428a2c88d2
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-21 16:07:41 +0545

    Update Binomial_distribution.py

--- Intro to Statisticcs in python/Binomial_distribution.py
+++ Intro to Statisticcs in python/Binomial_distribution.py
@@ -42,3 +42,26 @@ deals = binom.rvs(3,0.3,size=52)
 
 # Print mean deals won per week
 print(np.mean(deals))
+
+
+# ..................................................................................................................................................................
+
+# What's the probability that Amir closes all 3 deals in a week? Save this as prob_3.
+# Probability of closing 3 out of 3 deals
+prob_3 = binom.pmf(3,3,0.3)
+
+print(prob_3)
+# What's the probability that Amir closes 1 or fewer deals in a week? Save this as prob_less_than_or_equal_1.
+
+# Probability of closing <= 1 deal out of 3 deals
+prob_less_than_or_equal_1 = binom.cdf(1, 3, 0.3)
+
+print(prob_less_than_or_equal_1)
+
+# What's the probability that Amir closes more than 1 deal? Save this as prob_greater_than_1.
+
+
+# Probability of closing > 1 deal out of 3 deals
+prob_greater_than_1 = 1 - binom.cdf(1, 3, 0.3)
+
+print(prob_greater_than_1)
