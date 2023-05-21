c1c2ce9449987a8b64c10e5fec75cc30eade768c
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-21 16:33:13 +0545

    Create Normal_distribution.py

---
+++ Intro to Statisticcs in python/Normal_distribution.py
@@ -0,0 +1,5 @@
+# Create a histogram with 10 bins to visualize the distribution of the amount. Show the plot.
+# Histogram of amount with 10 bins and show plot
+amir_deals['amount'].hist(bins=10)
+plt.show()
+
