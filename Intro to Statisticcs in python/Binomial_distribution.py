dc602d84486036a2b0d741012f0cafa0a715502d
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-21 16:09:29 +0545

    Update Binomial_distribution.py

--- Intro to Statisticcs in python/Binomial_distribution.py
+++ Intro to Statisticcs in python/Binomial_distribution.py
@@ -65,3 +65,21 @@ print(prob_less_than_or_equal_1)
 prob_greater_than_1 = 1 - binom.cdf(1, 3, 0.3)
 
 print(prob_greater_than_1)
+
+
+# ..........................................................................................................................................................
+# Calculate the expected number of sales out of the 3 he works on that Amir will win each week if he maintains his 30% win rate.
+# Calculate the expected number of sales out of the 3 he works on that he'll win if his win rate drops to 25%.
+# Calculate the expected number of sales out of the 3 he works on that he'll win if his win rate rises to 35%.
+
+# Expected number won with 30% win rate
+won_30pct = 3 * 0.3
+print(won_30pct)
+
+# Expected number won with 25% win rate
+won_25pct = 3 * 0.25
+print(won_25pct)
+
+# Expected number won with 35% win rate
+won_35pct = 3 * 0.35
+print(won_35pct)
