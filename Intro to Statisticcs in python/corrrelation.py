e290385c312622bf84c59b01eb572646e4ffe0ca
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-22 16:39:50 +0545

    Create corrrelation.py

---
+++ Intro to Statisticcs in python/corrrelation.py
@@ -0,0 +1,28 @@
+# Create a scatterplot of happiness_score vs. life_exp (without a trendline) using seaborn.
+# Show the plot.
+
+# Create a scatterplot of happiness_score vs. life_exp and show
+sns.scatterplot(x='life_exp', y='happiness_score', data=world_happiness)
+
+# Show plot
+plt.show()
+
+# Create a scatterplot of happiness_score vs. life_exp with a linear trendline using seaborn, setting ci to None.
+# Show the plot.
+# Create scatterplot of happiness_score vs life_exp with trendline
+sns.lmplot(x='life_exp', y='happiness_score', data=world_happiness, ci=None)
+
+# Show plot
+plt.show()
+
+# Calculate the correlation between life_exp and happiness_score. Save this as cor.
+# Create scatterplot of happiness_score vs life_exp with trendline
+sns.lmplot(x='life_exp', y='happiness_score', data=world_happiness, ci=None)
+
+# Show plot
+plt.show()
+
+# Correlation between life_exp and happiness_score
+cor = world_happiness['life_exp'].corr(world_happiness['happiness_score'])
+
+print(cor)
