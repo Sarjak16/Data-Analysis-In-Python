fc51a017562513f441ea9186206d9ebfe644d4ec
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-22 16:50:42 +0545

    Update corrrelation.py

--- Intro to Statisticcs in python/corrrelation.py
+++ Intro to Statisticcs in python/corrrelation.py
@@ -47,3 +47,33 @@ plt.show()
 cor = cor = world_happiness['gdp_per_cap'].corr(world_happiness['life_exp'])
 
 print(cor)
+
+
+# transforming variables........................................................................................
+# Create a scatterplot of happiness_score versus gdp_per_cap and calculate the correlation between them.
+
+# Scatterplot of happiness_score vs. gdp_per_cap
+sns.scatterplot(x='gdp_per_cap', y='happiness_score', data=world_happiness)
+plt.show()
+
+
+# Calculate correlation
+cor = cor = world_happiness['gdp_per_cap'].corr(world_happiness['happiness_score'])
+print(cor)
+
+
+# Add a new column to world_happiness called log_gdp_per_cap that contains the log of gdp_per_cap.
+# Create a seaborn scatterplot of happiness_score versus log_gdp_per_cap.
+# Calculate the correlation between log_gdp_per_cap and happiness_scor
+
+# Create log_gdp_per_cap column
+world_happiness['log_gdp_per_cap'] = np.log(world_happiness['gdp_per_cap'])
+
+# Scatterplot of happiness_score vs. log_gdp_per_cap
+sns.scatterplot(x='log_gdp_per_cap', y='happiness_score', data=world_happiness)
+
+plt.show()
+
+# Calculate correlation
+cor = world_happiness['log_gdp_per_cap'].corr(world_happiness['happiness_score'])
+print(cor)
