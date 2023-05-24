2d61937c9c127f05cf8d10b9cee6e53bf9cecb22
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-24 19:19:56 +0545

    Update Plots.py

--- Intro to Data visualization with Seaborn/Plots.py
+++ Intro to Data visualization with Seaborn/Plots.py
@@ -26,3 +26,20 @@ sns.scatterplot(x=gdp, y=percent_literate)
 
 # Show plot
 plt.show()
+
+
+# countplot.....................................................................................................
+# Import Matplotlib and Seaborn using the standard naming conventions.
+# Use Seaborn to create a count plot with region on the y-axis.
+# Display the plot.
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+
+# Create count plot with region on the y-axis
+sns.countplot(y=region)
+
+# Show plot
+plt.show()
