6597c5a6779dfd99d9205830cb524a4bd0529f19
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-25 21:39:52 +0545

    Update customizing_plot.py

--- Intro to Data visualization with Seaborn/customizing_plot.py
+++ Intro to Data visualization with Seaborn/customizing_plot.py
@@ -52,3 +52,12 @@ sns.relplot(kind="line", x="model_year", y="mpg", data= mpg)
 
 # Show plot
 plt.show()
+
+
+# Change the plot so the shaded area shows the standard deviation instead of the confidence interval for the mean.
+# Make the shaded area show the standard deviation
+sns.relplot(x="model_year", y="mpg",
+            data=mpg, kind="line", ci="sd")
+
+# Show plot
+plt.show()
