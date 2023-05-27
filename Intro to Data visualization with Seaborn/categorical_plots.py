363d0c6aad2a5d200380a32d75208153aaac4af5
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-27 10:07:32 +0545

    Update categorical_plots.py

--- Intro to Data visualization with Seaborn/categorical_plots.py
+++ Intro to Data visualization with Seaborn/categorical_plots.py
@@ -31,3 +31,27 @@ sns.catplot(kind="bar", data=survey_data, x="Gender", y="Interested in Math")
 
 # Show plot
 plt.show()
+
+# Use sns.catplot() to create a bar plot with "study_time" on the x-axis and final grade ("G3") on the y-axis, using the student_data DataFrame.
+# Create bar plot of average final grade in each study category
+
+sns.catplot(kind="bar", x="study_time", y="G3", data= student_data)
+
+
+# Show plot
+plt.show()
+
+# Using the order parameter and the category_order list that is provided, rearrange the bars so that they are in order from lowest study time to highest
+# List of categories from lowest to highest
+category_order = ["<2 hours", 
+                  "2 to 5 hours", 
+                  "5 to 10 hours", 
+                  ">10 hours"]
+
+# Rearrange the categories
+sns.catplot(x="study_time", y="G3",
+            data=student_data,
+            kind="bar", order= category_order)
+
+# Show plot
+plt.show()
