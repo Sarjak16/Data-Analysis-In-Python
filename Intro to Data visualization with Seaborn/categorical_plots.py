eb15be23a4d1dd942a514367f35aa54a00d8cb82
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-27 10:03:13 +0545

    Update categorical_plots.py

--- Intro to Data visualization with Seaborn/categorical_plots.py
+++ Intro to Data visualization with Seaborn/categorical_plots.py
@@ -14,3 +14,11 @@ sns.catplot(y="Internet usage", data=survey_data,
 
 # Show plot
 plt.show()
+
+# Separate this plot into two side-by-side column subplots based on "Age Category", which separates respondents into those that are younger than 21 vs. 21 and older.
+# Separate into column subplots based on age category
+sns.catplot(y="Internet usage", data=survey_data,
+            kind="count", col="Age Category")
+
+# Show plot
+plt.show()
