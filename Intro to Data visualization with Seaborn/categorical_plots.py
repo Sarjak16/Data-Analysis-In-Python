a681fe0f963dd58a6752105ef2adf98bd9ad3800
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-27 10:09:15 +0545

    Update categorical_plots.py

--- Intro to Data visualization with Seaborn/categorical_plots.py
+++ Intro to Data visualization with Seaborn/categorical_plots.py
@@ -55,3 +55,19 @@ sns.catplot(x="study_time", y="G3",
 
 # Show plot
 plt.show()
+
+# Update the plot so that it no longer displays confidence intervals.
+# List of categories from lowest to highest
+category_order = ["<2 hours", 
+                  "2 to 5 hours", 
+                  "5 to 10 hours", 
+                  ">10 hours"]
+
+# Turn off the confidence intervals
+sns.catplot(x="study_time", y="G3",
+            data=student_data,
+            kind="bar",
+            order=category_order, ci=None)
+
+# Show plot
+plt.show()
