b5ea4aa0e3d93989f366d1a51e61e289ce862115
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-27 10:05:17 +0545

    Update categorical_plots.py

--- Intro to Data visualization with Seaborn/categorical_plots.py
+++ Intro to Data visualization with Seaborn/categorical_plots.py
@@ -22,3 +22,12 @@ sns.catplot(y="Internet usage", data=survey_data,
 
 # Show plot
 plt.show()
+
+# Bar plots....................................................................................................................
+# Use the survey_data DataFrame and sns.catplot() to create a bar plot with "Gender" on the x-axis and "Interested in Math" on the y-axis.
+# Create a bar plot of interest in math, separated by gender
+sns.catplot(kind="bar", data=survey_data, x="Gender", y="Interested in Math")
+
+
+# Show plot
+plt.show()
