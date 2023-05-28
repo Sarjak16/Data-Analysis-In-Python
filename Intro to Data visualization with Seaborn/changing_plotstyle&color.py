29af3701d33f04907558fb226635946b54e092cb
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-28 22:18:06 +0545

    Update changing_plotstyle&color.py

--- Intro to Data visualization with Seaborn/changing_plotstyle&color.py
+++ Intro to Data visualization with Seaborn/changing_plotstyle&color.py
@@ -45,4 +45,49 @@ sns.catplot(x="Parents Advice",
             order=category_order)
 
 # Show plot
+ plt.show()
+
+# Changing the scale....................................................................................................................
+# Set the scale ("context") to "paper", which is the smallest of the scale options.
+# Set the context to "paper"
+sns.set_context("paper")
+
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+
+# Show plot
+plt.show()
+
+# Change the context to "notebook" to increase the scale.
+# Change the context to "notebook"
+sns.set_context("notebook")
+
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+
+# Show plot
+plt.show()
+
+# Change the context to "talk" to increase the scale.
+# Change the context to "talk"
+sns.set_context("talk")
+
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+
+# Show plot
+plt.show()
+
+# Change the context to "poster", which is the largest scale available.
+# Change the context to "poster"
+sns.set_context("poster")
+
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+
+# Show plot
 plt.show()
