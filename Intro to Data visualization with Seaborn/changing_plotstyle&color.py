394e1ef327decc01a55b400fa748291b7d27c48d
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-28 22:04:35 +0545

    Create changing_plotstyle&color.py

---
+++ Intro to Data visualization with Seaborn/changing_plotstyle&color.py
@@ -0,0 +1,15 @@
+# Set the style to "whitegrid" to help the audience determine the number of responses in each category.
+# Set the style to "whitegrid"
+sns.set_style("whitegrid")
+
+# Create a count plot of survey responses
+category_order = ["Never", "Rarely", "Sometimes", 
+                  "Often", "Always"]
+
+sns.catplot(x="Parents Advice", 
+            data=survey_data, 
+            kind="count", 
+            order=category_order)
+
+# Show plot
+plt.show()
