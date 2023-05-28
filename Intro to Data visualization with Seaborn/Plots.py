32e9ae6274cd437bc23d58cc83eef87d3e4c3421
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-28 12:41:20 +0545

    Update Plots.py

--- Intro to Data visualization with Seaborn/Plots.py
+++ Intro to Data visualization with Seaborn/Plots.py
@@ -85,3 +85,22 @@ sns.catplot(x="romantic", y="G3",
 
 # Show plot
 plt.show()
+
+# Change the code to set the whiskers to extend to the 5th and 95th percentiles.
+# Extend the whiskers to the 5th and 95th percentile
+sns.catplot(x="romantic", y="G3",
+            data=student_data,
+            kind="box",
+            whis=[5, 95])
+
+# Show plot
+plt.show()
+
+# Set the whiskers at the min and max values
+sns.catplot(x="romantic", y="G3",
+            data=student_data,
+            kind="box",
+            whis=[0, 100])
+
+# Show plot
+plt.show()
