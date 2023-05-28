6f8471c9f46839a06bbc3de1f9872ddc8805530c
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-28 12:38:44 +0545

    Update Plots.py

--- Intro to Data visualization with Seaborn/Plots.py
+++ Intro to Data visualization with Seaborn/Plots.py
@@ -63,3 +63,15 @@ sns.catplot(x="study_time", y="G3", data= student_data, kind="box", order= study
 
 # Show plot
 plt.show()
+
+
+# Use sns.catplot() to create a box plot with the student_data DataFrame, putting "internet" on the x-axis and "G3" on the y-axis.
+# Add subgroups so each box plot is colored based on "location".
+# Do not display the outliers.
+
+# Create a box plot with subgroups and omit the outliers
+sns.catplot(x="internet", y="G3", data= student_data, hue="location", sym="", kind="box")
+
+
+# Show plot
+plt.show()
