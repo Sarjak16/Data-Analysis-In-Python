4dc6806858fbd6e23947a787ca6ce1e94bc639dd
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-28 21:55:04 +0545

    Update Plots.py

--- Intro to Data visualization with Seaborn/Plots.py
+++ Intro to Data visualization with Seaborn/Plots.py
@@ -131,4 +131,39 @@ sns.catplot(x="famrel", y="absences",
             capsize=0.2, join= False)
             
 # Show plot
+# plt.show()
+
+# Use sns.catplot() and the student_data DataFrame to create a point plot with relationship status ("romantic") on the x-axis and number of absences ("absences") on the y-axis.
+# Color the points based on the school that they attend ("school").
+# Create a point plot that uses color to create subgroups
+sns.catplot(kind="point", data=student_data, x="romantic", y="absences", hue="school")
+
+
+
+# Show plot
+plt.show()
+
+# Turn off the confidence intervals for this plot
+sns.catplot(x="romantic", y="absences",
+			data=student_data,
+            kind="point",
+            hue="school", ci=None)
+
+# Show plot
+plt.show()
+
+# Since there may be outliers of students with many absences, use the median function that
+# we've imported from numpy to display the median number of absences instead of the average.
+
+# Import median function from numpy
+from numpy import median
+
+# Plot the median number of absences instead of the mean
+sns.catplot(x="romantic", y="absences",
+			data=student_data,
+            kind="point",
+            hue="school",
+            ci=None, estimator= median)
+
+# Show plot
 plt.show()
