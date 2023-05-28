7bd1c8d4b197f33ac42bf4495384f1ab7f230293
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-28 12:35:42 +0545

    Update Plots.py

--- Intro to Data visualization with Seaborn/Plots.py
+++ Intro to Data visualization with Seaborn/Plots.py
@@ -43,3 +43,23 @@ sns.countplot(y=region)
 
 # Show plot
 plt.show()
+
+
+
+
+# BOX PLOTS.................................................................................................................................................
+# Use sns.catplot() and the student_data DataFrame to create a box plot with "study_time" on the x-axis and "G3" on the y-axis.
+# Set the ordering of the categories to study_time_order.
+
+# Specify the category ordering
+study_time_order = ["<2 hours", "2 to 5 hours", 
+                    "5 to 10 hours", ">10 hours"]
+
+# Create a box plot and set the order of the categories
+
+sns.catplot(x="study_time", y="G3", data= student_data, kind="box", order= study_time_order)
+
+
+
+# Show plot
+plt.show()
