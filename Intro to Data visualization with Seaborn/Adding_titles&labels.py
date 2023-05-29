14afbe57786edf2af19da00ac04256cbfed7a279
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-29 20:38:32 +0545

    Update Adding_titles&labels.py

--- Intro to Data visualization with Seaborn/Adding_titles&labels.py
+++ Intro to Data visualization with Seaborn/Adding_titles&labels.py
@@ -10,3 +10,17 @@ type_of_g = type(g)
 
 # Print type
 print(type_of_g)
+
+
+# Add the following title to this plot: "Car Weight vs. Horsepower".
+# Create scatter plot
+g = sns.relplot(x="weight", 
+                y="horsepower", 
+                data=mpg,
+                kind="scatter")
+
+# Add a title "Car Weight vs. Horsepower"
+
+g.fig.suptitle("Car Weight vs. Horsepower")
+# Show plot
+plt.show()
