0137b57b3ef8ac7c3289e6738db683385d9fb056
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-29 20:37:15 +0545

    Create Adding_titles&labels.py

---
+++ Intro to Data visualization with Seaborn/Adding_titles&labels.py
@@ -0,0 +1,12 @@
+# Identify what type of object plot g is and assign it to the variable type_of_g.
+# Create scatter plot
+g = sns.relplot(x="weight", 
+                y="horsepower", 
+                data=mpg,
+                kind="scatter")
+
+# Identify plot type
+type_of_g = type(g)
+
+# Print type
+print(type_of_g)
