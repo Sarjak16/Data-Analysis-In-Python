42ea0db0a5763a6a75283950a9837c035fbedee2
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-29 20:47:39 +0545

    Update Adding_titles&labels.py

--- Intro to Data visualization with Seaborn/Adding_titles&labels.py
+++ Intro to Data visualization with Seaborn/Adding_titles&labels.py
@@ -56,3 +56,21 @@ g.set(xlabel="Car Model Year", ylabel="Average MPG")
 
 # Show plot
 plt.show()
+
+# ...................................................................ROTATION.................................................................
+
+# # Rotate the x-tick labels 90 degrees.
+
+# Create point plot
+sns.catplot(x="origin", 
+            y="acceleration", 
+            data=mpg, 
+            kind="point", 
+            join=False, 
+            capsize=0.1)
+
+# Rotate x-tick labels
+plt.xticks(rotation= 90)
+
+# Show plot
+plt.show()
