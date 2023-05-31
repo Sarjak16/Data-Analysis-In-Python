ad7184d86ec1d494f0ff8595dead38b62182133b
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-31 20:51:21 +0545

    Update Initial_Exploration.py

--- Exploratory data analysis in python/Initial_Exploration.py
+++ Exploratory data analysis in python/Initial_Exploration.py
@@ -18,3 +18,14 @@ print(unemployment.describe())
 # Use a pandas function to count the values associated with each continent in the unemployment DataFrame.
 # Count the values associated with each continent in unemployment
 print(unemployment.continent.value_counts())
+
+
+# Import the required visualization libraries.
+# Create a histogram of the distribution of 2021 unemployment percentages across all countries in unemployment; show a full percentage point in each bin.
+# Import the required visualization libraries
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Create a histogram of 2021 unemployment; show a full percent in each bin
+sns.histplot(data=unemployment, x="2021", binwidth=1)
+plt.show()
