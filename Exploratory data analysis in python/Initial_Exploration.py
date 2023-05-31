2110c6bd01c9ab31a27e8e069f7c0508a6138b97
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-05-31 20:29:17 +0545

    Update Initial_Exploration.py

--- Exploratory data analysis in python/Initial_Exploration.py
+++ Exploratory data analysis in python/Initial_Exploration.py
@@ -1,1 +1,13 @@
-//Use a pandas function to print the first five rows of the unemployment DataFrame.
+# .............................................................................Functions for initial exploration........................................................
+
+# Use a pandas function to print the first five rows of the unemployment DataFrame.
+# Print the first five rows of unemployment
+print(unemployment.head())
+
+# Use a pandas function to print a summary of column non-missing values and data types from the unemployment DataFrame.
+# Print a summary of non-missing values and data types in the unemployment DataFrame
+print(unemployment.info())
+
+# Print the summary statistics (count, mean, standard deviation, min, max, and quartile values) of each numerical column in unemployment.
+# Print summary statistics for numerical columns in unemployment
+print(unemployment.describe())
