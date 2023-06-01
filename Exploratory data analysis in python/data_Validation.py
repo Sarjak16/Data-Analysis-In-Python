1aeaa6c3f0ddb1229422ec1d9c4d4da7feb0f390
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-01 20:39:22 +0545

    Update data_Validation.py

--- Exploratory data analysis in python/data_Validation.py
+++ Exploratory data analysis in python/data_Validation.py
@@ -5,3 +5,15 @@
 unemployment["2019"] = unemployment["2019"].astype(float)
 # Print the dtypes to check your work
 print(unemployment.head())
+
+# Define a Series of Booleans describing whether or not each continent is outside of Oceania; call this Series not_oceania.
+# Define a Series describing whether each continent is outside of Oceania
+not_oceania = ~unemployment["continent"].isin(["Oceania"])
+
+
+# Use Boolean indexing to print the unemployment DataFrame without any of the data related to countries in Oceania.
+# Define a Series describing whether each continent is outside of Oceania
+not_oceania = ~unemployment["continent"].isin(["Oceania"])
+
+# Print unemployment without records related to countries in Oceania
+print(unemployment[not_oceania])
