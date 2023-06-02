5cc14fcc46843066aac5c13fb55e89b466a85574
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-02 22:24:56 +0545

    Create Converting&Analyzing_categoricalData.py

---
+++ Exploratory data analysis in python/Converting&Analyzing_categoricalData.py
@@ -0,0 +1,6 @@
+# Filter planes for columns that are of "object" data type.
+# Loop through the columns in the dataset.
+# Add the column iterator to the print statement, then call the function to return the number of unique values in the column.
+# Create a list of categories
+flight_categories = ["Short-haul", "Medium", "Long-haul"]
+
