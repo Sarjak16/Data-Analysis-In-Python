2268535e48e55ef540a8e4fb1c0d8bfcdfdbe38b
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-03 21:46:44 +0545

    Update Working_with_numeric_Data.py

--- Exploratory data analysis in python/Working_with_numeric_Data.py
+++ Exploratory data analysis in python/Working_with_numeric_Data.py
@@ -32,3 +32,26 @@ planes["Duration"] = planes["Duration"].astype(float)
 # Plot a histogram
 sns.histplot(x="Duration", data=planes)
 plt.show()
+
+
+# Adding descreptive ststistics.................................................................................................................................
+# Add a column to planes containing the standard deviation of "Price" based on "Airline".
+# Price standard deviation by Airline
+planes["airline_price_st_dev"] = planes.groupby("Airline")["Price"].transform(lambda x: x.std())
+
+print(planes[["Airline", "airline_price_st_dev"]].value_counts())
+
+
+
+# Calculate the median for "Duration" by "Airline", storing it as a column called "airline_median_duration".
+# Median Duration by Airline
+planes["airline_median_duration"] = planes.groupby("Airline")["Duration"].transform(lambda x: x.median())
+
+print(planes[["Airline","airline_median_duration"]].value_counts())
+
+
+# Find the mean "Price" by "Destination", saving it as a column called "price_destination_mean".
+# Mean Price by Destination
+planes["price_destination_mean"] = planes.groupby("Destination")["Price"].transform(lambda x: x.mean())
+
+print(planes[["Destination","price_destination_mean"]].value_counts())
