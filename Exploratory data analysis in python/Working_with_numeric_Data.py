2b1d005c3f0c9424013ec18d9152b6dafab92b7d
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-03 21:38:01 +0545

    Update Working_with_numeric_Data.py

--- Exploratory data analysis in python/Working_with_numeric_Data.py
+++ Exploratory data analysis in python/Working_with_numeric_Data.py
@@ -1,4 +1,21 @@
-Print the first five values of the "Duration" column.
+# Print the first five values of the "Duration" column.
 
 # Preview the column
 print(planes["Duration"].head())
+
+# Remove "h" from the column.
+# Preview the column
+print(planes["Duration"].head())
+
+# Remove the string character
+planes["Duration"] = planes["Duration"].str.replace("h", "")
+
+# Convert the column to float data type
+# Preview the column
+print(planes["Duration"].head())
+
+# Remove the string character
+planes["Duration"] = planes["Duration"].str.replace("h", "")
+
+# Convert to float data type
+planes["Duration"] = planes["Duration"].astype(float)
