f3a58cbacfc463ff40ba30d187e3abcf64a2229e
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-04 14:01:15 +0545

    Update HAndling_Outliers.py

--- Exploratory data analysis in python/HAndling_Outliers.py
+++ Exploratory data analysis in python/HAndling_Outliers.py
@@ -11,3 +11,44 @@ plt.show()
 
 # Display descriptive statistics for flight duration
 print(planes['Duration'].describe())
+
+
+REMOVING OUTLIERS..................................................................................
+
+# Find the 75th and 25th percentiles, saving as price_seventy_fifth and price_twenty_fifth respectively.
+
+# Find the 75th and 25th percentiles
+price_seventy_fifth = planes["Price"].quantile(0.75)
+price_twenty_fifth = planes["Price"].quantile(0.25)
+
+# Calculate the IQR, storing it as prices_iqr.
+# Find the 75th and 25th percentiles
+price_upper_perc = planes["Price"].quantile(0.75)
+price_lower_perc = planes["Price"].quantile(0.25)
+
+# Calculate iqr
+prices_iqr = price_upper_perc - price_lower_perc
+# Calculate the upper and lower outlier thresholds.
+
+# Find the 75th and 25th percentiles
+price_seventy_fifth = planes["Price"].quantile(0.75)
+price_twenty_fifth = planes["Price"].quantile(0.25)
+
+# Calculate iqr
+prices_iqr = price_seventy_fifth - price_twenty_fifth
+
+# Calculate the thresholds
+upper = price_seventy_fifth + (1.5 * prices_iqr)
+lower = price_twenty_fifth - (1.5 * prices_iqr)
+# Remove the outliers from planes.
+# Calculate iqr
+prices_iqr = price_seventy_fifth - price_twenty_fifth
+
+# Calculate the thresholds
+upper = price_seventy_fifth + (1.5 * prices_iqr)
+lower = price_twenty_fifth - (1.5 * prices_iqr)
+
+# Subset the data
+planes = planes[(planes["Price"] > lower) & (planes["Price"] < upper)]
+
+print(planes["Price"].describe())
