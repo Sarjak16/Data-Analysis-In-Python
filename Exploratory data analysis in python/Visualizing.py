3841444ace4e145da14d9e2d4ec3e7b4444a1a2c
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-05 14:56:15 +0545

    Update Visualizing.py

--- Exploratory data analysis in python/Visualizing.py
+++ Exploratory data analysis in python/Visualizing.py
@@ -2,3 +2,10 @@
 # Create the scatterplot
 sns.scatterplot(x='marriage_duration', y='num_kids', data=divorce)
 plt.show()
+
+
+# Visualizing multiple variable relationships....................................................................................
+# Create a pairplot to visualize the relationships between income_woman and marriage_duration in the divorce DataFrame.
+# Create a pairplot for income_woman and marriage_duration
+sns.pairplot(data=divorce, vars=["income_woman", "marriage_duration" ])
+plt.show()
