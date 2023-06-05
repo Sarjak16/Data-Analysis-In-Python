6da91db021d251f7d56967c40065e1d26912058b
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-05 12:42:27 +0545

    Update Patterns_over_time.py

--- Exploratory data analysis in python/Patterns_over_time.py
+++ Exploratory data analysis in python/Patterns_over_time.py
@@ -12,3 +12,10 @@ divorce["marriage_date"] = pd.to_datetime(divorce["marriage_date"])
 # Define a column called marriage_year, which contains just the year portion of the marriage_date column.
 # Define the marriage_year column
 divorce["marriage_year"] = divorce['marriage_date'].dt.year
+
+# Define the marriage_year column
+divorce["marriage_year"] = divorce["marriage_date"].dt.year
+
+# Create a line plot showing the average number of kids by year
+sns.lineplot(data=divorce, y="num_kids", x="marriage_year")
+plt.show()
