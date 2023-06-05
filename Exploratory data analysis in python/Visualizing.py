c82a2b460198d6c81cfb4ccb3a038bbf8d048273
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-05 14:52:41 +0545

    Create Visualizing.py

---
+++ Exploratory data analysis in python/Visualizing.py
@@ -0,0 +1,4 @@
+# Create a scatterplot showing marriage_duration on the x-axis and num_kids on the y-axis.
+# Create the scatterplot
+sns.scatterplot(x='marriage_duration', y='num_kids', data=divorce)
+plt.show()
