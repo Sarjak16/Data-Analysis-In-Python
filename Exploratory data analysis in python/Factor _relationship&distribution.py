4c4ac26d2e4e15149c4fbc73a38262d730e6f407
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-06 22:05:55 +0545

    Update Factor _relationship&distribution.py

--- Exploratory data analysis in python/Factor _relationship&distribution.py
+++ Exploratory data analysis in python/Factor _relationship&distribution.py
@@ -7,5 +7,16 @@ plt.show()
 # Create a KDE plot that shows marriage_duration on the x-axis and a different colored line for each possible number of children that a couple might have,
 # represented by num_kids.
 # Update the KDE plot so that marriage duration can't be smoothed too far
-sns.kdeplot(data=divorce, x="marriage_duration", hue="num_kids", ____)
+# Create the KDE plot
+sns.kdeplot(data=divorce, x="marriage_duration", hue="num_kids", cut=0)
+plt.show()
+
+# Notice that the plot currently shows marriage durations less than zero; update the KDE plot so that marriage duration cannot be smoothed past the extreme data points.
+# Update the KDE plot so that marriage duration can't be smoothed too far
+sns.kdeplot(data=divorce, x="marriage_duration", hue="num_kids", cut=0)
+plt.show()
+
+# Update the code for the KDE plot from the previous step to show a cumulative distribution function for each number of children a couple has.
+# Update the KDE plot to show a cumulative distribution function
+sns.kdeplot(data=divorce, x="marriage_duration", hue="num_kids", cut=0, cumulative= True)
 plt.show()
