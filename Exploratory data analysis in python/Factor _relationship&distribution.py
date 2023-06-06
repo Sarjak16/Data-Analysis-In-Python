f1ec7721d0faff7da91e0d1cf4c6b0a710ad9f57
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-06 22:00:35 +0545

    Update Factor _relationship&distribution.py

--- Exploratory data analysis in python/Factor _relationship&distribution.py
+++ Exploratory data analysis in python/Factor _relationship&distribution.py
@@ -3,3 +3,9 @@
 # Create the scatter plot
 sns.scatterplot(data=divorce, x="woman_age_marriage", y="income_woman", hue="education_woman")
 plt.show()
+
+# Create a KDE plot that shows marriage_duration on the x-axis and a different colored line for each possible number of children that a couple might have,
+# represented by num_kids.
+# Update the KDE plot so that marriage duration can't be smoothed too far
+sns.kdeplot(data=divorce, x="marriage_duration", hue="num_kids", ____)
+plt.show()
