c203fef1145622144dbf6c58e5d400b28f1067fd
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-07 22:31:14 +0545

    Update Generating_new_Features.py

--- Exploratory data analysis in python/Generating_new_Features.py
+++ Exploratory data analysis in python/Generating_new_Features.py
@@ -26,3 +26,40 @@ salaries_median = salaries["Salary_USD"].median()
 # Gather the 75th percentile
 seventy_fifth = salaries["Salary_USD"].quantile(.75)
 print(twenty_fifth, salaries_median, seventy_fifth)
+
+# Categorizing.................................................................
+# Create salary_labels, a list containing "entry", "mid", "senior", and "exec".
+# Create salary labels
+salary_labels = ["entry", "mid", "senior", "exec"]
+# Finish salary_ranges, adding the 25th percentile, median, 75th percentile, and largest value from "Salary_USD".
+# Create salary labels
+salary_labels = ["entry", "mid", "senior", "exec"]
+
+# Create the salary ranges list
+salary_ranges = [0, twenty_fifth, salaries_median, seventy_fifth, salaries["Salary_USD"].max()]
+# Split "Salary_USD" based on the labels and ranges you've created.
+# Create salary labels
+salary_labels = ["entry", "mid", "senior", "exec"]
+
+# Create the salary ranges list
+salary_ranges = [0, twenty_fifth, salaries_median, seventy_fifth, salaries["Salary_USD"].max()]
+
+# Create salary_level
+salaries["salary_level"] = pd.cut(salaries["Salary_USD"],
+                                  bins=salary_ranges,
+                                  labels=salary_labels)
+# Use sns.countplot() to visualize the count of "Company_Size", factoring salary level labels.
+# Create salary labels
+salary_labels = ["entry", "mid", "senior", "exec"]
+
+# Create the salary ranges list
+salary_ranges = [0, twenty_fifth, salaries_median, seventy_fifth, salaries["Salary_USD"].max()]
+
+# Create salary_level
+salaries["salary_level"] = pd.cut(salaries["Salary_USD"],
+                                  bins=salary_ranges,
+                                  labels=salary_labels)
+
+# Plot the count of salary levels at companies of different sizes
+sns.countplot(data=salaries, x="Company_Size", hue="salary_level")
+plt.show()
