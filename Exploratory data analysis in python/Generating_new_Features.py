11abe179f9c778652018b20ebfbe0530173e3661
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-07 22:19:17 +0545

    Update Generating_new_Features.py

--- Exploratory data analysis in python/Generating_new_Features.py
+++ Exploratory data analysis in python/Generating_new_Features.py
@@ -12,3 +12,17 @@ salaries["weekday"] = salaries["date_of_response"].dt.weekday
 # Create a heatmap
 sns.heatmap(salaries.corr(), annot=True)
 plt.show()
+
+# Calculating Percentiles............................................
+# Find the 25th percentile of "Salary_USD".
+# Store the median of "Salary_USD" as salaries_median.
+# Get the 75th percentile of salaries.
+# Find the 25th percentile
+twenty_fifth = salaries["Salary_USD"].quantile(.25)
+
+# Save the median
+salaries_median = salaries["Salary_USD"].median()
+
+# Gather the 75th percentile
+seventy_fifth = salaries["Salary_USD"].quantile(.75)
+print(twenty_fifth, salaries_median, seventy_fifth)
