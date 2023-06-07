074a9cf0a5e854c76cf31b6ac046830cfe538661
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-07 22:46:01 +0545

    Update and rename Generating_hypothesis.py\ to Generating_hypothesis.py

--- Exploratory data analysis in python/Generating_hypothesis.py
+++ Exploratory data analysis in python/Generating_hypothesis.py
@@ -7,3 +7,9 @@ usa_and_gb = salaries[salaries["Employee_Location"].isin(["US", "GB"])]
 # Create a barplot of salaries by location
 sns.barplot(data=usa_and_gb, x="Employee_Location", y="Salary_USD")
 plt.show()
+
+# Produce a barplot comparing "Salary_USD" by "Company_Size", factoring "Employment_Status".
+# Create a bar plot of salary versus company size, factoring in employment status
+sns.barplot(data=salaries, x="Company_Size", y="Salary_USD", hue="Employment_Status")
+plt.show()
+
