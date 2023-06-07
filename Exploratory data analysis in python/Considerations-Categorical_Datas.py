33828dce34754ae158dddbbf814d704d7d4a6105
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-07 21:57:16 +0545

    Update Considerations-Categorical_Datas.py

--- Exploratory data analysis in python/Considerations-Categorical_Datas.py
+++ Exploratory data analysis in python/Considerations-Categorical_Datas.py
@@ -1,3 +1,17 @@
 # Print the relative frequency of the "Job_Category" column from salaries DataFrame.
 # Print the relative frequency of Job_Category
 print(salaries["Job_Category"].value_counts(normalize=True))
+
+
+#Cross Tabulation.............................................................................................................
+# Perform cross-tabulation, setting "Company_Size" as the index, and the columns to classes in "Experience".
+# Cross-tabulate Company_Size and Experience
+print(pd.crosstab(salaries["Company_Size"], salaries["Experience"]))
+
+# Cross-tabulate "Job_Category" and classes of "Company_Size" as column names.
+# Cross-tabulate Job_Category and Company_Size
+print(pd.crosstab(salaries["Job_Category"], salaries["Company_Size"]))
+# Update pd.crosstab() to return the mean "Salary_USD" values.
+# Cross-tabulate Job_Category and Company_Size
+print(pd.crosstab(salaries["Job_Category"], salaries["Company_Size"],
+            values=salaries["Salary_USD"], aggfunc="mean"))
