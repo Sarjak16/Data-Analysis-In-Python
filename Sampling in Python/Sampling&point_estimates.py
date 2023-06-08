169d352d620dda85fdad9387a666fce3e666c9d7
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-08 20:28:40 +0545

    Update Sampling&point_estimates.py

--- Sampling in Python/Sampling&point_estimates.py
+++ Sampling in Python/Sampling&point_estimates.py
@@ -23,3 +23,16 @@ mean_dur_samp = spotify_sample['duration_minutes'].mean()
 # Print the means
 print(mean_dur_pop)
 print(mean_dur_samp)
+
+# SAMPLING with NUMPY................................................................
+# Create a pandas Series, loudness_pop, by subsetting the loudness column from spotify_population.
+# Sample loudness_pop to get 100 random values, assigning to loudness_samp.
+# Create a pandas Series from the loudness column of spotify_population
+loudness_pop = pd.Series(spotify_population['loudness'])
+
+# Sample 100 values of loudness_pop
+loudness_samp = loudness_pop.sample(n=100)
+
+# Print the sample
+print(loudness_samp)
+
