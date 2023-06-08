85e0ab315b2ee2a9ca72b29157f4e7f53b5638a0
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-08 20:25:46 +0545

    Update Sampling&point_estimates.py

---
+++ Sampling in Python/Sampling&point_estimates.py
@@ -0,0 +1,10 @@
+# SIMPLE SAMPLING WITH PANDAS.....................................................................................................................................
+# Sample 1000 rows from spotify_population, assigning to spotify_sample.
+# Sample 1000 rows from spotify_population
+spotify_sample = spotify_population.sample(n=1000)
+
+# Print the sample
+print(spotify_sample)
+
+# Calculate the mean duration in minutes from spotify_population using pandas.
+# Calculate the mean duration in minutes from spotify_sample using pandas.
