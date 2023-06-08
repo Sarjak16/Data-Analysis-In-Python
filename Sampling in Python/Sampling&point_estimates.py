30bba1da978282e75206d27d51641e035a0cf160
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-08 20:30:20 +0545

    Update Sampling&point_estimates.py

--- Sampling in Python/Sampling&point_estimates.py
+++ Sampling in Python/Sampling&point_estimates.py
@@ -36,3 +36,20 @@ loudness_samp = loudness_pop.sample(n=100)
 # Print the sample
 print(loudness_samp)
 
+# Calculate the mean of loudness_pop using numpy.
+# Calculate the mean of loudness_samp using numpy.
+# Create a pandas Series from the loudness column of spotify_population
+loudness_pop = spotify_population['loudness']
+
+# Sample 100 values of loudness_pop
+loudness_samp = loudness_pop.sample(n=100)
+
+# Calculate the mean of loudness_pop
+mean_loudness_pop = loudness_pop.mean()
+
+# Calculate the mean of loudness_samp
+mean_loudness_samp = loudness_samp.mean()
+
+# Print the means
+print(mean_loudness_pop)
+print(mean_loudness_samp)
