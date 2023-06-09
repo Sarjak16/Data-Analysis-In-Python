f374c5adf3eefb7a576f0691a2c4110ced13572e
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-09 22:27:46 +0545

    Update Convinience_sampling.py

--- Sampling in Python/Convinience_sampling.py
+++ Sampling in Python/Convinience_sampling.py
@@ -2,3 +2,8 @@
 # Visualize the distribution of acousticness with a histogram
 spotify_population['acousticness'].hist(bins=np.arange(0, 1.01, 0.01))
 plt.show()
+
+# Update the histogram code to use the spotify_mysterious_sample dataset.
+# Update the histogram to use spotify_mysterious_sample
+spotify_mysterious_sample['acousticness'].hist(bins=np.arange(0, 1.01, 0.01))
+plt.show()
