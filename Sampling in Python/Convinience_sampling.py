6ee0c2e6937e127274df75f9e873e9b9e331230f
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-09 22:32:06 +0545

    Update Convinience_sampling.py

--- Sampling in Python/Convinience_sampling.py
+++ Sampling in Python/Convinience_sampling.py
@@ -7,3 +7,13 @@ plt.show()
 # Update the histogram to use spotify_mysterious_sample
 spotify_mysterious_sample['acousticness'].hist(bins=np.arange(0, 1.01, 0.01))
 plt.show()
+
+# Plot a histogram of duration_minutes from spotify_population with bins of width 0.5 from 0 to 15 using pandas .hist().
+# Visualize the distribution of duration_minutes as a histogram
+spotify_population['duration_minutes'].hist(bins= np.arange(0, 15.5, 0.5))
+plt.show()
+
+# Update the histogram code to use the spotify_mysterious_sample2 dataset.
+# Update the histogram to use spotify_mysterious_sample2
+spotify_mysterious_sample2['duration_minutes'].hist(bins=np.arange(0, 15.5, 0.5))
+plt.show()
