cc751bbf52d912080307dda541258e16fa985181
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-10 20:44:22 +0545

    Update RAndom_number.py

--- Sampling in Python/RAndom_number.py
+++ Sampling in Python/RAndom_number.py
@@ -15,3 +15,19 @@ normals = np.random.normal(loc=5, scale=2, size=5000)
 
 # Print normals
 print(normals)
+
+# Plot a histogram of uniforms with bins of width of 0.25 from -3 to 3 using plt.hist().
+# Generate random numbers from a Uniform(-3, 3)
+uniforms = np.random.uniform(low=-3, high=3, size=5000)
+
+# Plot a histogram of uniform values, binwidth 0.25
+plt.hist(uniforms, bins=np.arange(-3, 3.25, 0.25))
+plt.show()
+
+# Plot a histogram of normals with bins of width of 0.5 from -2 to 13 using plt.hist().
+# Generate random numbers from a Normal(5, 2)
+normals = np.random.normal(loc=5, scale=2, size=5000)
+
+# Plot a histogram of normal values, binwidth 0.5
+plt.hist(normals, bins= np.arange(-2, 13.5, 0.5))
+plt.show()
