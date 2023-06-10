e7c814897b537a259472ffdffff24f9b0456c5f9
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-10 20:39:23 +0545

    Create RAndom_number.py

---
+++ Sampling in Python/RAndom_number.py
@@ -0,0 +1,17 @@
+# Generate 5000 numbers from a uniform distribution, setting the parameters low to -3 and high to 3.
+# Generate random numbers from a Uniform(-3, 3)
+uniforms = np.random.uniform(low=-3, high=3, size=5000)
+
+# Print uniforms
+print(uniforms)
+
+# Generate 5000 numbers from a normal distribution, setting the parameters loc to 5 and scale to 2.
+
+# Generate random numbers from a Uniform(-3, 3)
+uniforms = np.random.uniform(low=-3, high=3, size=5000)
+
+# Generate random numbers from a Normal(5, 2)
+normals = np.random.normal(loc=5, scale=2, size=5000)
+
+# Print normals
+print(normals)
