5b60bb79440cb42a99812a8c5bce31a352ebd465
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-10 21:18:32 +0545

    Update RAndom_number.py

--- Sampling in Python/RAndom_number.py
+++ Sampling in Python/RAndom_number.py
@@ -85,3 +85,26 @@ attrition_sys_samp = attrition_pop.iloc[::interval]
 
 # Print the sample
 print(attrition_sys_samp)
+
+
+# Add an index column to attrition_pop, assigning the result to attrition_pop_id.
+# Create a scatter plot of YearsAtCompany versus index for attrition_pop_id using pandas .plot().
+# Add an index column to attrition_pop
+attrition_pop_id = attrition_pop.reset_index()
+
+# Plot YearsAtCompany vs. index for attrition_pop_id
+attrition_pop_id.plot(kind='scatter', x='index', y='YearsAtCompany')
+plt.show()
+
+# Randomly shuffle the rows of attrition_pop.
+# Reset the row indexes, and add an index column to attrition_pop.
+# Shuffle the rows of attrition_pop
+attrition_shuffled = attrition_pop.sample(frac=1)
+
+# Reset the row indexes and create an index column
+attrition_shuffled = attrition_shuffled.reset_index(drop=True).reset_index()
+
+# Plot YearsAtCompany vs. index for attrition_shuffled
+attrition_shuffled.plot(kind="scatter", x="index", y="YearsAtCompany")
+plt.show()
+# Repeat the scatter plot of YearsAtCompany versus index, this time using attrition_shuffled.
