2856552702abcb903dd26ee89dc6c114987fd942
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-11 17:39:56 +0545

    Update Stratified&_Weighted_randomsampling.py

--- Sampling in Python/Stratified&_Weighted_randomsampling.py
+++ Sampling in Python/Stratified&_Weighted_randomsampling.py
@@ -61,3 +61,30 @@ print(education_counts_eq)
 
 # WEIGHTED Sampling.............................................................................................................................................
 # Plot YearsAtCompany from attrition_pop as a histogram with bins of width 1 from 0 to 40.
+# Plot YearsAtCompany from attrition_pop as a histogram
+attrition_pop['YearsAtCompany'].hist(bins=np.arange(0, 41, 1))
+plt.show()
+
+# Sample 400 employees from attrition_pop weighted by YearsAtCompany.
+# Plot YearsAtCompany from attrition_pop as a histogram
+attrition_pop['YearsAtCompany'].hist(bins=np.arange(0, 41, 1))
+plt.show()
+
+# Sample 400 employees weighted by YearsAtCompany
+attrition_weight =  attrition_pop.sample(n=400, weights='YearsAtCompany')
+
+# Print the sample
+print(attrition_weight)
+
+# Plot YearsAtCompany from attrition_weight as a histogram with bins of width 1 from 0 to 40.
+
+# Plot YearsAtCompany from attrition_pop as a histogram
+attrition_pop['YearsAtCompany'].hist(bins=np.arange(0, 41, 1))
+plt.show()
+
+# Sample 400 employees weighted by YearsAtCompany
+attrition_weight = attrition_pop.sample(n=400, weights="YearsAtCompany")
+
+# Plot YearsAtCompany from attrition_weight as a histogram
+attrition_weight['YearsAtCompany'].hist(bins=np.arange(0, 41, 1))
+plt.show()
