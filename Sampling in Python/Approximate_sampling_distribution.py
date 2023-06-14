e41096d23877983dbfc45e14239c23a8f123366c
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-14 20:45:07 +0545

    Update Approximate_sampling_distribution.py

--- Sampling in Python/Approximate_sampling_distribution.py
+++ Sampling in Python/Approximate_sampling_distribution.py
@@ -32,3 +32,23 @@ dice['mean_roll'] = dice['mean_roll'].astype('category')
 
 # Print result
 print(dice)
+
+# Create a bar plot of the mean_roll categorical column, so it displays the count of each mean_roll in increasing order from 1.0 to 8.0.
+# Expand a grid representing 5 8-sided dice
+dice = expand_grid(
+  {'die1': [1, 2, 3, 4, 5, 6, 7, 8],
+   'die2': [1, 2, 3, 4, 5, 6, 7, 8],
+   'die3': [1, 2, 3, 4, 5, 6, 7, 8],
+   'die4': [1, 2, 3, 4, 5, 6, 7, 8],
+   'die5': [1, 2, 3, 4, 5, 6, 7, 8]
+  })
+
+# Add a column of mean rolls and convert to a categorical
+dice['mean_roll'] = (dice['die1'] + dice['die2'] + 
+                     dice['die3'] + dice['die4'] + 
+                     dice['die5']) / 5
+dice['mean_roll'] = dice['mean_roll'].astype('category')
+
+# Draw a bar plot of mean_roll
+dice['mean_roll'].value_counts(sort=False).plot(kind='bar', rot=0)
+plt.show()
