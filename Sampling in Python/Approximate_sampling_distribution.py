d3767eaf3255354bb4def2413bd3376a7bc9cf11
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-14 21:57:46 +0545

    Update Approximate_sampling_distribution.py

--- Sampling in Python/Approximate_sampling_distribution.py
+++ Sampling in Python/Approximate_sampling_distribution.py
@@ -52,3 +52,23 @@ dice['mean_roll'] = dice['mean_roll'].astype('category')
 # Draw a bar plot of mean_roll
 dice['mean_roll'].value_counts(sort=False).plot(kind='bar', rot=0)
 plt.show()
+
+# Generating approx sampling................................................................................
+# Sample one to eight, five times, with replacement. Assign to five_rolls.
+# Calculate the mean of five_rolls.
+# Sample one to eight, five times, with replacement
+five_rolls = np.random.choice(a=range(1, 9), size=5, replace=True)
+
+# Print the mean of five_rolls
+print(np.mean(five_rolls))
+
+# Replicate the sampling code 1000 times, assigning each result to the list sample_means_1000.
+# Replicate the sampling code 1000 times
+sample_means_1000 = []
+for i in range(1000):
+    sample_means_1000.append(
+  		np.random.choice(list(range(1, 9)), size=5, replace=True).mean()
+    )
+    
+# Print the first 10 entries of the result
+print(sample_means_1000[0:10])
