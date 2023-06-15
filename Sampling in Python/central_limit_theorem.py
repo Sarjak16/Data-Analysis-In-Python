3439cc05fe582c99f64daafe4c68ff173a959d7a
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-15 21:36:00 +0545

    Update central_limit_theorem.py

--- Sampling in Python/central_limit_theorem.py
+++ Sampling in Python/central_limit_theorem.py
@@ -10,3 +10,15 @@ mean_of_means_500 = np.mean(sampling_distribution_500)
 print(mean_of_means_5)
 print(mean_of_means_50)
 print(mean_of_means_500)
+
+
+# Calculate the standard deviation of sampling_distribution_5, sampling_distribution_50, and sampling_distribution_500 (a standard deviation of sample means).
+# Calculate the std. dev. of the mean attritions for each sampling distribution
+sd_of_means_5 = np.std(sampling_distribution_5, ddof=1)
+sd_of_means_50 = np.std(sampling_distribution_50, ddof=1)
+sd_of_means_500 = np.std(sampling_distribution_500, ddof=1)
+
+# Print the results
+print(sd_of_means_5)
+print(sd_of_means_50)
+print(sd_of_means_500)
