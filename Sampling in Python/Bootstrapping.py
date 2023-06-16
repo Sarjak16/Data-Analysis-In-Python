082065ca24a288c5c69297ce917ba6455e31bb56
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-16 21:56:01 +0545

    Update Bootstrapping.py

--- Sampling in Python/Bootstrapping.py
+++ Sampling in Python/Bootstrapping.py
@@ -6,3 +6,14 @@ spotify_1_resample = spotify_sample.sample(n=len(spotify_sample), replace=True)
 # Print the resample
 print(spotify_1_resample)
 
+# Calculate the mean of the danceability column of spotify_1_resample using numpy.
+# Generate 1 bootstrap resample
+spotify_1_resample = spotify_sample.sample(frac=1, replace=True)
+
+# Calculate of the danceability column of spotify_1_resample
+mean_danceability_1 = np.mean(spotify_1_resample['danceability'])
+
+# Print the result
+print(mean_danceability_1)
+
+# Replicate the expression provided 1000 times.
