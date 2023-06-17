d44c87acd677ee1cd04af6a201540f72a016c964
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-17 21:16:37 +0545

    Update standard_error.py

--- Sampling in Python/standard_error.py
+++ Sampling in Python/standard_error.py
@@ -12,3 +12,18 @@ for i in range (2000):
 
 # Print the sampling distribution results
 print(mean_popularity_2000_samp)
+
+
+# Generate a bootstrap distribution of 2000 replicates.
+# Sample 500 rows of the sample with replacement and calculate the mean popularity.
+mean_popularity_2000_boot = []
+
+# Generate a bootstrap distribution of 2000 replicates
+for i in range (2000):
+    mean_popularity_2000_boot.append(
+    	# Resample 500 rows and calculate the mean popularity     
+    	spotify_sample.sample(n=500, replace=True)['popularity'].mean()
+    )
+
+# Print the bootstrap distribution results
+print(mean_popularity_2000_boot)
