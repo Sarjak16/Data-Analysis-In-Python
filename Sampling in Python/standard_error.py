974c4d5543c6710ebe697f308a35afdfa286db3d
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-17 21:20:19 +0545

    Update standard_error.py

--- Sampling in Python/standard_error.py
+++ Sampling in Python/standard_error.py
@@ -27,3 +27,25 @@ for i in range (2000):
 
 # Print the bootstrap distribution results
 print(mean_popularity_2000_boot)
+
+
+# Calculate the mean popularity in 4 ways:
+
+# Population: from spotify_population, take the mean of popularity.
+# Sample: from spotify_sample, take the mean of popularity.
+# Sampling distribution: from sampling_distribution, take its mean.
+# Bootstrap distribution: from bootstrap_distribution, take its mean.
+# Calculate the population mean popularity
+pop_mean = np.mean(spotify_population['popularity'])
+
+# Calculate the original sample mean popularity
+samp_mean = np.mean(spotify_sample['popularity'])
+
+# Calculate the sampling dist'n estimate of mean popularity
+samp_distn_mean = np.mean(sampling_distribution)
+
+# Calculate the bootstrap dist'n estimate of mean popularity
+boot_distn_mean = np.mean(bootstrap_distribution)
+
+# Print the means
+print([pop_mean, samp_mean, samp_distn_mean, boot_distn_mean])
