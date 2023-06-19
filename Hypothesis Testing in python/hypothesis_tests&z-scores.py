7fbc9363a7c49f28696457da949cdbc2e8b4b720
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-19 21:20:36 +0545

    Update hypothesis_tests&z-scores.py

--- Hypothesis Testing in python/hypothesis_tests&z-scores.py
+++ Hypothesis Testing in python/hypothesis_tests&z-scores.py
@@ -1,4 +1,4 @@
-# Calculating the sample mean
+# Calculating the sample mean.........................................................................................
 # Print the late_shipments dataset.
 # Print the late_shipments dataset
 print(late_shipments)
@@ -12,3 +12,19 @@ late_prop_samp =(late_shipments["late"] == "Yes").mean()
 
 # Print the results
 print(late_prop_samp)
+
+# Calculating a z-score..............................................................................................
+# Hypothesize that the proportion of late shipments is 6%.
+# Calculate the standard error from the standard deviation of the bootstrap distribution.
+# Calculate the z-score.
+# Hypothesize that the proportion is 6%
+late_prop_hyp = 0.06
+
+# Calculate the standard error
+std_error = np.std(late_shipments_boot_distn)
+
+# Find z-score of late_prop_samp
+z_score = (late_prop_samp - late_prop_hyp) / std_error
+
+# Print z_score
+print(z_score)
