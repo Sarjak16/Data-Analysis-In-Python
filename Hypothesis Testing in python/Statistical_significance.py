555956e758c05a7c5ff925d060dd0063bd518d21
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-20 21:27:37 +0545

    Update Statistical_significance.py

--- Hypothesis Testing in python/Statistical_significance.py
+++ Hypothesis Testing in python/Statistical_significance.py
@@ -8,6 +8,25 @@ upper = np.quantile(late_shipments_boot_distn, 0.975)
 print((lower, upper))
 
 
+# T-test............................................................................................
+# The sample means for the two groups are available as xbar_no and xbar_yes. The sample standard deviations are s_no and s_yes.
+# The sample sizes are n_no and n_yes. numpy is also loaded as np.
 
+# Calculate the numerator of the 
+#  test statistic.
+# Calculate the denominator of the 
+#  test statistic.
+# Use those two numbers to calculate the 
+#  test statistic.
 
+# Calculate the numerator of the test statistic
+numerator = xbar_yes - xbar_no
 
+# Calculate the denominator of the test statistic
+denominator = np.sqrt(((s_no**2) / n_no) + ((s_yes**2) / n_yes))
+
+# Calculate the test statistic
+t_stat = numerator/denominator
+
+# Print the test statistic
+print(t_stat)
