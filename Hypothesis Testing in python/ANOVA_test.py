3bcf748d7e79f4d0cea8ce5ce220e1288be8add4
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-22 21:16:43 +0545

    Update ANOVA_test.py

--- Hypothesis Testing in python/ANOVA_test.py
+++ Hypothesis Testing in python/ANOVA_test.py
@@ -28,3 +28,15 @@ s_pack_by_mode = late_shipments.groupby("shipment_mode")['pack_price'].std()
 # Boxplot of shipment_mode vs. pack_price
 sns.boxplot(x="pack_price", y="shipment_mode", data=late_shipments)
 plt.show()
+
+
+
+#Conducting an ANOVA test..........................................................................................
+#Run an ANOVA on late_shipments investigating 'pack_price' (the dependent variable) between the groups of 'shipment_mode'.
+# Run an ANOVA for pack_price across shipment_mode
+anova_results = pingouin.anova(data=late_shipments, dv="pack_price", between="shipment_mode")
+
+
+
+# Print anova_results
+print(anova_results)
