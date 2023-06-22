9bea0a13f77aca77c4e8dc453541f82bf636faa0
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-22 20:51:25 +0545

    Update Testing.py

--- Hypothesis Testing in python/Testing.py
+++ Hypothesis Testing in python/Testing.py
@@ -7,3 +7,21 @@ test_results = pingouin.ttest(x=sample_dem_data['diff'], y=0, alternative="two-s
                             
 # Print the test results
 print(test_results)
+
+
+# Paired ttest()............................................................................................
+# Conduct a paired test on the democratic votes in 2012 and 2016 (the dem_percent_12 and dem_percent_16 columns of sample_dem_data),
+# using an appropriate alternative hypothesis.
+# Conduct a t-test on diff
+test_results = pingouin.ttest(x=sample_dem_data['diff'], 
+                              y=0, 
+                              alternative="two-sided")
+
+# Conduct a paired t-test on dem_percent_12 and dem_percent_16
+paired_test_results = pingouin.ttest(x=sample_dem_data['dem_percent_12'], y=sample_dem_data['dem_percent_16'], paired= True, alternative="two-sided")
+
+
+
+                              
+# Print the paired test results
+print(paired_test_results)
