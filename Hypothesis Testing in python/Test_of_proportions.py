678d55ae63302478e511d6b3306fddc5a6bf7e51
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-23 21:31:14 +0545

    Create Test_of_proportions.py

---
+++ Hypothesis Testing in python/Test_of_proportions.py
@@ -0,0 +1,7 @@
+# Calculate the pooled sample proportion, 
+# , from p_hats and ns.
+# Calculate the pooled estimate of the population proportion
+p_hat = (p_hats["reasonable"] * ns["reasonable"] + p_hats["expensive"] * ns["expensive"]) / (ns["reasonable"] + ns["expensive"])
+
+# Print the result
+print(p_hat)
