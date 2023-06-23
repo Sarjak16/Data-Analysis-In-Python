0c9d05ff5b67a09f0542b8f6801dd456a8c425b4
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-23 21:24:11 +0545

    Update One_sample_proportion_dist.py

--- Hypothesis Testing in python/One_sample_proportion_dist.py
+++ Hypothesis Testing in python/One_sample_proportion_dist.py
@@ -31,3 +31,27 @@ z_score = numerator/denominator
 
 # Print the result
 print(z_score)
+
+
+# Transform the z-score into a p-value, remembering that this is a "greater than" alternative hypothesis.
+# Hypothesize that the proportion of late shipments is 6%
+p_0 = 0.06
+
+# Calculate the sample proportion of late shipments
+p_hat = (late_shipments['late'] == "Yes").mean()
+
+# Calculate the sample size
+n = len(late_shipments)
+
+# Calculate the numerator and denominator of the test statistic
+numerator = p_hat - p_0
+denominator = np.sqrt(p_0 * (1 - p_0) / n)
+
+# Calculate the test statistic
+z_score = numerator / denominator
+
+# Calculate the p-value from the z-score
+p_value = 1- norm.cdf(z_score) 
+
+# Print the p-value
+print(p_value)
