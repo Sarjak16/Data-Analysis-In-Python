d31a30cb11eed6cf8f75719366a427344c346d1c
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-23 21:35:58 +0545

    Update Test_of_proportions.py

--- Hypothesis Testing in python/Test_of_proportions.py
+++ Hypothesis Testing in python/Test_of_proportions.py
@@ -5,3 +5,25 @@ p_hat = (p_hats["reasonable"] * ns["reasonable"] + p_hats["expensive"] * ns["exp
 
 # Print the result
 print(p_hat)
+
+
+# Calculate the standard error of the sample using this equation.
+# Calculate p_hat multiplied by (1 - p_hat).
+# Divide p_hat_times_not_p_hat by the number of "reasonable" rows and by the number of "expensive" rows, and sum those two values.
+# Calculate std_error by taking the square root of p_hat_times_not_p_hat_over_ns.
+
+
+# Calculate the pooled estimate of the population proportion
+p_hat = (p_hats["reasonable"] * ns["reasonable"] + p_hats["expensive"] * ns["expensive"]) / (ns["reasonable"] + ns["expensive"])
+
+# Calculate p_hat one minus p_hat
+p_hat_times_not_p_hat = p_hat * (1 - p_hat)
+
+# Divide this by each of the sample sizes and then sum
+p_hat_times_not_p_hat_over_ns = p_hat_times_not_p_hat / ns["expensive"] + p_hat_times_not_p_hat / ns["reasonable"]
+
+# Calculate the standard error
+std_error = np.sqrt(p_hat_times_not_p_hat_over_ns)
+
+# Print the result
+print(std_error)
