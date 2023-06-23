53dab8d1e7812eab6c871943a1a2158711e1bc1e
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-23 21:20:51 +0545

    Update One_sample_proportion_dist.py

--- Hypothesis Testing in python/One_sample_proportion_dist.py
+++ Hypothesis Testing in python/One_sample_proportion_dist.py
@@ -12,3 +12,22 @@ n = len(late_shipments)
 
 # Print p_hat and n
 print(p_hat, n)
+
+
+# Calculate the numerator and denominator of the z-score.
+# Calculate the z-score as the ratio of these numbers.
+# Calculate the sample proportion of late shipments
+p_hat = (late_shipments['late'] == "Yes").mean()
+
+# Calculate the sample size
+n = len(late_shipments)
+
+# Calculate the numerator and denominator of the test statistic
+numerator = p_hat-p_0
+denominator = np.sqrt(p_0 * (1- p_0)/n)
+
+# Calculate the test statistic
+z_score = numerator/denominator
+
+# Print the result
+print(z_score)
