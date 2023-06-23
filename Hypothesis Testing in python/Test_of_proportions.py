5ade81b1eef9f7d801e9a762003de211219991f7
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-23 21:46:01 +0545

    Update Test_of_proportions.py

--- Hypothesis Testing in python/Test_of_proportions.py
+++ Hypothesis Testing in python/Test_of_proportions.py
@@ -84,3 +84,23 @@ late_by_freight_cost_group =  late_shipments.groupby("freight_cost_group")["late
 
 # Print the counts
 print(late_by_freight_cost_group)
+
+
+# Extract the number of "Yes"'s for the two freight_cost_group into a numpy array, specifying the 'expensive' count and then 'reasonable'.
+# Determine the overall number of rows in each freight_cost_group as a numpy array, specifying the 'expensive' count and then 'reasonable'.
+# Run a z-test using proportions_ztest(), specifying alternative as "larger"
+# Count the late column values for each freight_cost_group
+late_by_freight_cost_group = late_shipments.groupby("freight_cost_group")['late'].value_counts()
+
+# Create an array of the "Yes" counts for each freight_cost_group
+success_counts = np.array([45, 16])
+
+# Create an array of the total number of rows in each freight_cost_group
+n = np.array([45 + 500, 16 + 439])
+
+# Run a z-test on the two proportions
+stat, p_value = proportions_ztest(count=success_counts, nobs=n,
+                                  alternative="larger")
+
+# Print the results
+print(stat, p_value)
