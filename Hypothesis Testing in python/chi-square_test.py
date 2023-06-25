b66e5fa4e5c12c002fcdcaea8b0eb9b54d182e66
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-25 21:59:00 +0545

    Update chi-square_test.py

--- Hypothesis Testing in python/chi-square_test.py
+++ Hypothesis Testing in python/chi-square_test.py
@@ -67,3 +67,15 @@ hypothesized['n'] = hypothesized['prop'] * n_total
 
 # Print the modified hypothesized DataFrame
 print(hypothesized)
+
+# Create a bar graph of 'n' versus 'vendor_inco_term' for the incoterm_counts data, specifying a red color.
+# Find the number of rows in late_shipments
+n_total = len(late_shipments)
+
+# Create n column that is prop column * n_total
+hypothesized["n"] = hypothesized["prop"] * n_total
+
+# Plot a red bar graph of n vs. vendor_inco_term for incoterm_counts
+plt.bar(incoterm_counts['vendor_inco_term'], incoterm_counts['n'], color="red", label="Observed")
+plt.legend()
+plt.show()
