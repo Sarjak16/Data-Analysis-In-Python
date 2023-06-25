3a6c5c87b3da706cea6043ae5bf9e64718806ddf
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-25 21:25:49 +0545

    Update chi-square_test.py

--- Hypothesis Testing in python/chi-square_test.py
+++ Hypothesis Testing in python/chi-square_test.py
@@ -4,3 +4,14 @@ props = late_shipments.groupby('vendor_inco_term')['freight_cost_group'].value_c
 
 # Print props
 print(props)
+
+
+# Unstack the .value_counts() result to be in wide format instead of long.
+# Proportion of freight_cost_group grouped by vendor_inco_term
+props = late_shipments.groupby('vendor_inco_term')['freight_cost_group'].value_counts(normalize=True)
+
+# Convert props to wide format
+wide_props = props.unstack()
+
+# Print wide_props
+print(wide_props)
