ae9c870683e239610986b3132bdf893062c37a57
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-26 18:26:13 +0545

    Create Assumption_in_hypothesis_test.py

---
+++ Hypothesis Testing in python/Assumption_in_hypothesis_test.py
@@ -0,0 +1,10 @@
+# late_shipments is available, and pandas is loaded as pd.Get the count of each value in the freight_cost_group column of late_shipments.
+# Insert a suitable number to inspect whether the counts are "big enough" for a two sample t-test
+# Count the freight_cost_group values
+counts = late_shipments["freight_cost_group"].value_counts()
+
+# Print the result
+print(counts)
+
+# Inspect whether the counts are big enough
+print((counts >= 30).all())
