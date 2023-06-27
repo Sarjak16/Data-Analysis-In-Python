541a40dc65f9f4e4a9106c83e4202a60152bd70b
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-27 20:38:35 +0545

    Update non_parametric_test.py

--- Hypothesis Testing in python/non_parametric_test.py
+++ Hypothesis Testing in python/non_parametric_test.py
@@ -5,3 +5,11 @@ paired_test_results = pingouin.ttest(x=sample_dem_data['dem_percent_12'], y=samp
 
 # Print paired t-test results
 print(paired_test_results)
+
+
+# Conduct a Wilcoxon-signed rank test on the same columns.
+# Conduct a Wilcoxon test on dem_percent_12 and dem_percent_16
+wilcoxon_test_results = pingouin.wilcoxon(sample_dem_data["dem_percent_12"], sample_dem_data["dem_percent_16"])
+
+# Print Wilcoxon test results
+print(wilcoxon_test_results)
