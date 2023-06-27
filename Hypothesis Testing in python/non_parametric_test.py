2b55c0d05f2de1c1d53dc770aa0c89c5915a5bcf
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-06-27 20:58:15 +0545

    Update non_parametric_test.py

--- Hypothesis Testing in python/non_parametric_test.py
+++ Hypothesis Testing in python/non_parametric_test.py
@@ -13,3 +13,24 @@ wilcoxon_test_results = pingouin.wilcoxon(sample_dem_data["dem_percent_12"], sam
 
 # Print Wilcoxon test results
 print(wilcoxon_test_results)
+
+
+
+# Wilcon-mann-whitney............................................................................................
+# Select weight_kilograms and late from late_shipments, assigning the name weight_vs_late.
+# Convert weight_vs_late from long-to-wide format, setting columns to 'late'.
+# Run a Wilcoxon-Mann-Whitney test for a difference in weight_kilograms when the shipment was late and on-time.
+# Select the weight_kilograms and late columns
+weight_vs_late = late_shipments[["weight_kilograms", "late"]]
+
+# Convert weight_vs_late into wide format
+weight_vs_late_wide = weight_vs_late.pivot(columns='late', 
+                                           values='weight_kilograms')
+
+# Run a two-sided Wilcoxon-Mann-Whitney test on weight_kilograms vs. late
+wmw_test = pingouin.mwu(x=weight_vs_late_wide['No'],
+                        y=weight_vs_late_wide['Yes'],
+                        alternative='two-sided')
+
+# Print the test results
+print(wmw_test)
