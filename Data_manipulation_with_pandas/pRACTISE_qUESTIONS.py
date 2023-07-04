6c7d9025b4b36f4b070b469cb1ebddba013252d2
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-04 22:24:30 +0545

    Update pRACTISE_qUESTIONS.py

--- Data_manipulation_with_pandas/pRACTISE_qUESTIONS.py
+++ Data_manipulation_with_pandas/pRACTISE_qUESTIONS.py
@@ -8,3 +8,20 @@
 
 print(churn.index())
 
+# The salesdataset contains information about daily store purchases. The filtered_salesdataset contains products that belong to Health and beautyand Sports. 
+
+# . 
+#           date       product_line     product  unit_price  quantity
+# 0   2018-01-15  Health and beauty     Shampoo        6.99         7
+# 3   2018-01-16             Sports    Yoga mat       39.99         5
+# 5   2018-01-17  Health and beauty     Shampoo       12.99         4
+# 6   2018-01-17             Sports    Yoga mat       29.99         3
+
+filtered_sales = sales[sales["product_line"].isin(["Health and beauty", "Sports"])]
+print(filtered_sales.groupby([
+'product_line'
+, 
+'product'
+])[
+'unit_price'
+].min())
