7ec61f6bdace2b467a4bfa7d07648cd4fb54e10c
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-04 22:21:15 +0545

    Create pRACTISE_qUESTIONS.py

---
+++ Data_manipulation_with_pandas/pRACTISE_qUESTIONS.py
@@ -0,0 +1,10 @@
+# # Thechurndataset contains customers' information and whether or not they exited the company. What is the correct way to display the row labels? 
+#     surname  credit_score geography  age exited
+# 0  Hargrave           619    France   42    Yes
+# 1      Hill           608     Spain   41     No
+# 2      Onio           502    France   42    Yes
+# 3      Boni           699    France   39     No
+# 4  Mitchell           850     Spain   43     No
+
+print(churn.index())
+
